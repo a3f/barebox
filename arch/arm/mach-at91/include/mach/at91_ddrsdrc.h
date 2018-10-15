d407ea0ea297a4648eb6102c89a55b89ddbe586a
Ahmad Fatoum <a.fatoum@pengutronix.de>
2018-10-15 17:18:16 +0200

    ARM: at91: replace at91sam9_ddrsdr.h with at91bootstrap's at91_ddrsdrc.h
    Instead of adding missing definitions to the existing at91sam9_ddrsdr.h
    and adapting the incoming DDRAM initialization code from at91bootstrap,
    just replace the lightly used existing header with:
    https://github.com/linux4sam/at91bootstrap/blob/v3.8.12/include/arch/at91_ddrsdrc.h
    
    For easier comprehension, the replacement is done in three steps:
    This last step copies the memory size querying functions from at91sam9_ddrsdr.h
    to at91_ddrsdrc.h, then deletes it and fixes all references.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

--- arch/arm/mach-at91/include/mach/at91_ddrsdrc.h
+++ arch/arm/mach-at91/include/mach/at91_ddrsdrc.h
@@ -285,4 +285,48 @@
 #define AT91C_DDRC2_WPVS	(0x1UL << 0)
 #define AT91C_DDRC2_WPSRC	(0xFFFFUL << 8)
 
+#ifndef __ASSEMBLY__
+#include <common.h>
+#include <io.h>
+
+static inline u32 at91_get_ddram_size(void * __iomem base, bool is_nb)
+{
+	u32 cr;
+	u32 mdr;
+	u32 size;
+	bool is_sdram;
+
+	cr = readl(base + AT91C_HDDRSDRC2_CR);
+	mdr = readl(base + AT91C_HDDRSDRC2_MDR);
+
+	/* will always be false for sama5d2, sama5d3 or sama5d4 */
+	is_sdram = (mdr & AT91C_DDRC2_MD) <= AT91C_DDRC2_MD_LP_SDR_SDRAM;
+
+	/* Formula:
+	 * size = bank << (col + row + 1);
+	 * if (bandwidth == 32 bits)
+	 *	size <<= 1;
+	 */
+	size = 1;
+	/* COL */
+	size += (cr & AT91C_DDRC2_NC) + 8;
+	if (!is_sdram)
+		size ++;
+	/* ROW */
+	size += ((cr & AT91C_DDRC2_NR) >> 2) + 11;
+	/* BANK */
+	if (is_nb)
+		size = ((cr & AT91C_DDRC2_NB_BANKS) ? 8 : 4) << size;
+	else
+		size = 4 << size;
+
+	/* bandwidth */
+	if (!(mdr & AT91C_DDRC2_DBW))
+		size <<= 1;
+
+	return size;
+}
+
+#endif /* __ASSEMBLY__ */
+
 #endif /* __AT91_DDRSDRC_H__ */
