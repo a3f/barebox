71bc94c6b9b8826707967a114cc6f2fe10a268a1
Ahmad Fatoum <a.fatoum@pengutronix.de>
2018-10-15 19:54:56 +0200

    ARM: at91: import low level DDRAMC initialization code from at91bootstrap
    This commit imports DDRAMC initialization routines for use in PBL from
    https://github.com/linux4sam/at91bootstrap/blob/v3.8.12/driver/ddramc.c
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

---
+++ arch/arm/mach-at91/include/mach/ddramc.h
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: BSD-1-Clause
+/*
+ * Copyright (c) 2006, Atmel Corporation
+ */
+#ifndef __DDRAMC_H__
+#define __DDRAMC_H__
+
+struct at91_ddramc_register {
+	unsigned long mdr;
+	unsigned long cr;
+	unsigned long rtr;
+	unsigned long t0pr;
+	unsigned long t1pr;
+	unsigned long t2pr;
+	unsigned long lpr;
+	unsigned long lpddr2_lpr;
+	unsigned long tim_calr;
+	unsigned long cal_mr4r;
+};
+
+void at91_ddram_initialize(unsigned long base_address,
+			   unsigned long ram_address,
+			   struct at91_ddramc_register *ddramc_config);
+
+void at91_lpddr2_sdram_initialize(unsigned long base_address,
+				  unsigned long ram_address,
+				  struct at91_ddramc_register *ddramc_config);
+
+
+void at91_lpddr1_sdram_initialize(unsigned long base_address,
+				  unsigned long ram_address,
+				  struct at91_ddramc_register *ddramc_config);
+
+
+#endif /* #ifndef __DDRAMC_H__ */
