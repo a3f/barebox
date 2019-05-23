84961a952c1725cea436310871cce743700ba883
Ahmad Fatoum <a.fatoum@pengutronix.de>
2019-05-24 00:24:36 +0200

    ARM: imx6: add Embest MarSBoard support
    This board is produced by Embest/Element 14 and is based on i.MX6 Dual
    The following features are tested :
        - UART2 (console)
        - eMMC
        - uSDCard
        - Ethernet
        - USB Host (through 2 ports hub)
        - I2C 1/2
        - SPI NOR Flash
    
    Boot from eMMC boot partition and through USB loader are tested.
    
    For more informations on this board :
    http://www.embest-tech.com/shop/star/marsboard.html
    
    Signed-off-by: Eric Bénard <eric@eukrea.com>
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

---
+++ arch/arm/boards/embest-marsboard/board.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2014 Eric Bénard <eric@eukrea.com>
+ * Copyright (C) 2013 Lucas Stach <l.stach@pengutronix.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <common.h>
+#include <init.h>
+#include <mach/bbu.h>
+#include <linux/phy.h>
+
+static int ar8035_phy_fixup(struct phy_device *dev)
+{
+	u16 val;
+
+	/* Ar803x phy SmartEEE feature cause link status generates glitch,
+	 * which cause ethernet link down/up issue, so disable SmartEEE
+	 */
+	phy_write(dev, 0xd, 0x3);
+	phy_write(dev, 0xe, 0x805d);
+	phy_write(dev, 0xd, 0x4003);
+
+	val = phy_read(dev, 0xe);
+	phy_write(dev, 0xe, val & ~(1 << 8));
+
+	/* To enable AR8031 ouput a 125MHz clk from CLK_25M */
+	phy_write(dev, 0xd, 0x7);
+	phy_write(dev, 0xe, 0x8016);
+	phy_write(dev, 0xd, 0x4007);
+
+	val = phy_read(dev, 0xe);
+	val &= 0xffe3;
+	val |= 0x18;
+	phy_write(dev, 0xe, val);
+
+	/* introduce tx clock delay */
+	phy_write(dev, 0x1d, 0x5);
+	val = phy_read(dev, 0x1e);
+	val |= 0x0100;
+	phy_write(dev, 0x1e, val);
+
+	return 0;
+}
+
+static int marsboard_device_init(void)
+{
+	if (!of_machine_is_compatible("embest,imx6q-marsboard"))
+		return 0;
+
+	phy_register_fixup_for_uid(0x004dd072, 0xffffffef, ar8035_phy_fixup);
+
+	imx6_bbu_internal_mmcboot_register_handler("emmc", "/dev/mmc3",
+			BBU_HANDLER_FLAG_DEFAULT);
+
+	barebox_set_hostname("marsboard");
+
+	return 0;
+}
+device_initcall(marsboard_device_init);
---
+++ arch/arm/boards/embest-marsboard/lowlevel.c
@@ -0,0 +1,26 @@
+#include <debug_ll.h>
+#include <common.h>
+#include <linux/sizes.h>
+#include <io.h>
+#include <asm/barebox-arm-head.h>
+#include <asm/barebox-arm.h>
+#include <mach/imx6.h>
+
+extern char __dtb_imx6q_marsboard_start[];
+
+ENTRY_FUNCTION(start_imx6q_marsboard, r0, r1, r2)
+{
+	void *fdt;
+
+	imx6_cpu_lowlevel_init();
+
+	if (IS_ENABLED(CONFIG_DEBUG_LL)) {
+		writel(0x4, 0x020e016c);
+		imx6_uart_setup_ll();
+		putc_ll('a');
+	}
+
+	fdt = __dtb_imx6q_marsboard_start + get_runtime_offset();
+
+	barebox_arm_entry(0x10000000, SZ_1G, fdt);
+}
