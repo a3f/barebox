aab1ba3029eb6d6c1451be71dc4a61ce6c75e01b
Ahmad Fatoum <a.fatoum@pengutronix.de>
2019-05-25 17:04:38 +0200

    ARM: stm32mp: stm32mp157c-dk2: configure debug_ll UART
    This configures UART4 for use as the debug_ll UART,
    whenever CONFIG_DEBUG_LL is defined.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

--- arch/arm/boards/stm32mp157c-dk2/lowlevel.c
+++ arch/arm/boards/stm32mp157c-dk2/lowlevel.c
@@ -5,6 +5,24 @@
 #include <mach/stm32.h>
 #include <debug_ll.h>
 
+#define RCC_MP_APB1ENSETR (STM32_RCC_BASE + 0x0A00)
+#define RCC_MP_AHB4ENSETR (STM32_RCC_BASE + 0x0A28)
+#define GPIOG_BASE 0x50008000
+
+static inline void setup_uart(void)
+{
+	/* UART4 clock enable */
+	setbits_le32(RCC_MP_APB1ENSETR, BIT(16));
+
+	/* GPIOG clock enable */
+	writel(BIT(6), RCC_MP_AHB4ENSETR);
+	/* GPIO configuration for EVAL board => UART4 TX = G11 */
+	writel(0xffbfffff, GPIOG_BASE + 0x00);
+	writel(0x00006000, GPIOG_BASE + 0x24);
+
+	putc_ll('>');
+}
+
 extern char __dtb_stm32mp157c_dk2_start[];
 
 ENTRY_FUNCTION(start_stm32mp157c_dk2, r0, r1, r2)
@@ -12,6 +30,9 @@ ENTRY_FUNCTION(start_stm32mp157c_dk2, r0, r1, r2)
 	void *fdt;
 
 	arm_cpu_lowlevel_init();
+
+	if (IS_ENABLED(CONFIG_DEBUG_LL))
+		setup_uart();
 
 	fdt = __dtb_stm32mp157c_dk2_start + get_runtime_offset();
 
