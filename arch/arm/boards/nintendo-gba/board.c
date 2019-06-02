f1560a38f3ebeb4d2360c3ef3e919b6940c478c5
Ahmad Fatoum <a.fatoum@pengutronix.de>
2019-06-02 19:11:29 +0200

    fixup! ARM: add Nintendo Gameboy Advance (GBA) Support

--- arch/arm/boards/nintendo-gba/board.c
+++ arch/arm/boards/nintendo-gba/board.c
@@ -5,6 +5,8 @@
 
 #include <bootsource.h>
 #include <common.h>
+#include <mach/gba.h>
+#include <asm/memory.h>
 #include <init.h>
 
 static int gba_device_init(void)
@@ -17,3 +19,14 @@ static int gba_device_init(void)
 	return 0;
 }
 device_initcall(gba_device_init);
+
+static int gba_postcore_init(void)
+{
+	if (!of_machine_is_compatible("nintendo,gameboy-advance"))
+                return 0;
+
+        arm_add_mem_device("ram0", GBA_EWRAM_BASE, GBA_EWRAM_SIZE);
+
+        return 0;
+}
+mem_initcall(gba_postcore_init);
