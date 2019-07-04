e171c5c280b6def6a25688d06deeabb5f8b1b017
Ahmad Fatoum <a.fatoum@pengutronix.de>
2019-07-05 01:02:48 +0200

    misc: implement PSCI client driver
    System reset on the STM32MP may be done via PSCI when running TF-A
    as first-stage boot loader. Provide a PSCI client driver to handle
    this generically.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

---
+++ drivers/misc/psci.c
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2017 Masahiro Yamada <yamada.masahiro@socionext.com>
+ * Copyright (C) 2019 Ahmad Fatoum, Pengutronix
+ */
+
+#include <common.h>
+#include <init.h>
+#include <driver.h>
+#include <asm/psci.h>
+#include <asm/secure.h>
+#include <linux/arm-smccc.h>
+#include <command.h>
+#include <getopt.h>
+
+static u32 invoke_psci_too_early(ulong function,
+				 ulong arg0, ulong arg1, ulong arg2)
+{
+	panic("fatal: invoke_psci() called before sucessful psci_probe()");
+}
+
+static u32 (*psci_invoke_fn)(ulong, ulong, ulong, ulong) = invoke_psci_too_early;
+
+u32 psci_invoke(ulong function, ulong arg0, ulong arg1, ulong arg2)
+{
+	return psci_invoke_fn(function, arg0, arg1, arg2);
+}
+
+static u32 invoke_psci_fn_hvc(ulong function,
+			      ulong arg0, ulong arg1, ulong arg2)
+{
+	struct arm_smccc_res res;
+	arm_smccc_hvc(function, arg0, arg1, arg2, 0, 0, 0, 0, &res);
+	return (long)res.a0;
+}
+
+static u32 invoke_psci_fn_smc(ulong function,
+			      ulong arg0, ulong arg1, ulong arg2)
+{
+	struct arm_smccc_res res;
+	arm_smccc_smc(function, arg0, arg1, arg2, 0, 0, 0, 0, &res);
+	return res.a0;
+}
+
+static int __init psci_probe(struct device_d *dev)
+{
+	const char *method;
+	const char *version;
+	const void *param;
+	int ret;
+
+	ret = dev_get_drvdata(dev, (const void **)&version);
+	if (ret)
+		return -ENODEV;
+
+	ret = of_property_read_string(dev->device_node, "method", &method);
+	if (ret) {
+		dev_warn(dev, "missing \"method\" property\n");
+		return -ENXIO;
+	}
+
+	if (!strcmp(method, "hvc")) {
+		psci_invoke_fn = invoke_psci_fn_hvc;
+	} else if (!strcmp(method, "smc")) {
+		psci_invoke_fn = invoke_psci_fn_smc;
+	} else {
+		pr_warn("invalid \"method\" property: %s\n", method);
+		return -EINVAL;
+	}
+
+	param = dev_add_param_string_fixed(dev, "version", version);
+	return PTR_ERR_OR_ZERO(param);
+}
+
+static __maybe_unused struct of_device_id psci_dt_ids[] = {
+	{ .compatible = "arm,psci", .data = "0.1" },
+	{ .compatible = "arm,psci-0.2", .data = "0.2" },
+	{ .compatible = "arm,psci-1.0", .data = "1.0" },
+	{ /* sentinel */ },
+};
+
+static struct driver_d psci_driver = {
+	.name = "psci",
+	.probe = psci_probe,
+	.of_compatible = DRV_OF_COMPAT(psci_dt_ids),
+};
+coredevice_platform_driver(psci_driver);
