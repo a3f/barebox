eea4aecd004ef9f134abc3a7eafa32d90d58f81f
Ahmad Fatoum <a.fatoum@pengutronix.de>
2019-07-05 01:04:26 +0200

    misc: implement PSCI system reset driver
    In cases where firmware provides PSCI >0.1, it may be prudent to use it as
    backend for reset and poweroff. This driver accomplishes this.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

---
+++ drivers/misc/psci-sysreset.c
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2017 Masahiro Yamada <yamada.masahiro@socionext.com>
+ * Copyright (C) 2019 Ahmad Fatoum, Pengutronix
+ */
+
+#define pr_fmr(fmt) "psci-sysreset: " fmt
+
+#include <common.h>
+#include <init.h>
+#include <driver.h>
+#include <asm/psci.h>
+#include <poweroff.h>
+#include <restart.h>
+
+static struct restart_handler restart;
+
+static void __noreturn psci_invoke_noreturn(int function)
+{
+	int ret;
+
+	ret = psci_invoke(function, 0, 0, 0, NULL);
+
+	pr_err("psci command failed: %s\n", strerror(-ret));
+	hang();
+}
+
+static void __noreturn psci_poweroff(struct poweroff_handler *handler)
+{
+	psci_invoke_noreturn(ARM_PSCI_0_2_FN_SYSTEM_OFF);
+}
+
+static void __noreturn psci_restart(struct restart_handler *rst)
+{
+	psci_invoke_noreturn(ARM_PSCI_0_2_FN_SYSTEM_RESET);
+}
+
+static int __init psci_sysreset_init(void)
+{
+	struct device_d *dev;
+	const char *version;
+	int ret;
+
+	dev = get_device_by_name("psci.of");
+	if (!dev)
+		return -ENODEV;
+
+	version = dev_get_param(dev, "version");
+	if (!version)
+		return -ENXIO;
+
+	if (strcmp(version, "0.1") == 0)
+		return 0;
+
+	ret = poweroff_handler_register_fn(psci_poweroff);
+	if (ret)
+		return ret;
+
+	restart.name = "psci";
+	restart.restart = psci_restart;
+	restart.priority = 400;
+
+	return restart_handler_register(&restart);
+}
+device_initcall(psci_sysreset_init);
