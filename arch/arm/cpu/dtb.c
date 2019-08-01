88131876c3e047f7ca6c540c5e37ddd4f4025e26
Ahmad Fatoum <a.fatoum@pengutronix.de>
2019-08-01 10:51:10 +0200

    [DEBUGGING] add debug output in of_arm_inti

--- arch/arm/cpu/dtb.c
+++ arch/arm/cpu/dtb.c
@@ -16,15 +16,18 @@
  */
 #include <common.h>
 #include <init.h>
+#include <debug_ll.h>
 #include <of.h>
 #include <asm/barebox-arm.h>
 
+#define DEBUG 1
 extern char __dtb_start[];
 
 static int of_arm_init(void)
 {
 	struct device_node *root;
 	void *fdt;
+	pr_info("uaaaasing boarddata provided DTB\n");
 
 	/* See if we already have a dtb */
 	root = of_get_root_node();
@@ -34,26 +37,34 @@ static int of_arm_init(void)
 	/* See if we are provided a dtb in boarddata */
 	fdt = barebox_arm_boot_dtb();
 	if (fdt)
-		pr_debug("using boarddata provided DTB\n");
+		pr_info("using boarddata provided DTB\n");
 
 	/* Next see if we have a builtin dtb */
 	if (!fdt && IS_ENABLED(CONFIG_BUILTIN_DTB)) {
 		fdt = __dtb_start;
-		pr_debug("using internal DTB\n");
+		pr_info("using internal DTB\n");
 	}
 
 	if (!fdt) {
-		pr_debug("No DTB found\n");
+		pr_info("No DTB found\n");
 		return 0;
 	}
 
+	puts_ll("0\n");
 	root = of_unflatten_dtb(fdt);
+	puts_ll("1\n");
 	if (!IS_ERR(root)) {
+	puts_ll("2\n");
 		of_set_root_node(root);
+	puts_ll("3\n");
 		of_fix_tree(root);
+	puts_ll("4\n");
 		if (IS_ENABLED(CONFIG_OFDEVICE))
 			of_probe();
+	puts_ll("5\n");
 	}
+
+	puts_ll("all well\n");
 
 	return 0;
 }
