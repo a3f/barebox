7f31d8fdc196ecc79a7cfc8c8de7854068d9f2ec
Ahmad Fatoum <a.fatoum@pengutronix.de>
2019-08-12 20:01:31 +0200

    ARM: at91: sama5d27_som1: add board code for first stage
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

---
+++ arch/arm/boards/sama5d27-som1/board.c
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2019 Ahmad Fatoum, Pengutronix
+ */
+
+#include <init.h>
+#include <mach/board.h>
+#include <mach/iomux.h>
+
+static int sama5d27_som1_ek_mem_init(void)
+{
+	at91_add_device_sdram(0);
+
+	return 0;
+}
+mem_initcall(sama5d27_som1_ek_mem_init);
+
+static int sama5d27_som1_ek_devices_init(void)
+{
+	// TODO add MMC
+	return 0;
+}
+device_initcall(sama5d27_som1_ek_devices_init);
