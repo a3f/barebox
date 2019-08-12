b1007f3ca3857e2feeae9df58dcc86aa9a7e4af2
Ahmad Fatoum <a.fatoum@pengutronix.de>
2019-08-12 20:01:31 +0200

    ARM: at91: sama5d27_som1: add SDRAM initialization
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

--- arch/arm/boards/sama5d27-som1/lowlevel.c
+++ arch/arm/boards/sama5d27-som1/lowlevel.c
@@ -1,5 +1,6 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-2.0-only AND BSD-1-Clause
 /*
+ * Copyright (C) 2014, Atmel Corporation
  * Copyright (C) 2019 Ahmad Fatoum, Pengutronix
  */
 
@@ -8,12 +9,14 @@
 
 #include <asm/barebox-arm-head.h>
 #include <asm/barebox-arm.h>
-#include <mach/at91_pmc_ll.h>
+#include <mach/sama5d2_ll.h>
 
-#include <mach/hardware.h>
 #include <mach/iomux.h>
 #include <debug_ll.h>
 #include <mach/at91_dbgu.h>
+#include <mach/at91_ddrsdrc.h>
+#include <mach/at91_wdt.h>
+#include <mach/gpio.h>
 
 #define RGB_LED_GREEN (1 << 0)
 #define RGB_LED_RED   (1 << 1)
@@ -21,9 +24,6 @@
 
 /* PCK = 492MHz, MCK = 164MHz */
 #define MASTER_CLOCK	164000000
-
-#define sama5d2_pmc_enable_periph_clock(clk) \
-	at91_pmc_sam9x5_enable_periph_clock(IOMEM(SAMA5D2_BASE_PMC), clk)
 
 static void ek_turn_led(unsigned color)
 {
@@ -61,20 +61,86 @@ static void ek_dbgu_init(void)
 	putc_ll('>');
 }
 
+static struct at91_ddramc_register ddramc_reg_config = {
+	.mdr = AT91C_DDRC2_DBW_16_BITS |
+		AT91C_DDRC2_MD_DDR2_SDRAM,
+
+	.cr = AT91C_DDRC2_NC_DDR10_SDR9 |
+		AT91C_DDRC2_NR_13 |
+		AT91C_DDRC2_CAS_3 |
+		AT91C_DDRC2_DISABLE_RESET_DLL |
+		AT91C_DDRC2_WEAK_STRENGTH_RZQ7 |
+		AT91C_DDRC2_ENABLE_DLL |
+		AT91C_DDRC2_NB_BANKS_8 |
+		AT91C_DDRC2_NDQS_ENABLED |
+		AT91C_DDRC2_DECOD_INTERLEAVED |
+		AT91C_DDRC2_UNAL_SUPPORTED,
+
+	.rtr = 0x511,
+
+	.t0pr = AT91C_DDRC2_TRAS_(7) |
+		AT91C_DDRC2_TRCD_(3) |
+		AT91C_DDRC2_TWR_(3) |
+		AT91C_DDRC2_TRC_(9) |
+		AT91C_DDRC2_TRP_(3) |
+		AT91C_DDRC2_TRRD_(2) |
+		AT91C_DDRC2_TWTR_(2) |
+		AT91C_DDRC2_TMRD_(2),
+
+	.t1pr = AT91C_DDRC2_TRFC_(22) |
+		AT91C_DDRC2_TXSNR_(23) |
+		AT91C_DDRC2_TXSRD_(200) |
+		AT91C_DDRC2_TXP_(2),
+
+	.t2pr = AT91C_DDRC2_TXARD_(2) |
+		AT91C_DDRC2_TXARDS_(8) |
+		AT91C_DDRC2_TRPA_(4) |
+		AT91C_DDRC2_TRTP_(2) |
+		AT91C_DDRC2_TFAW_(8),
+};
+
+static void noinline __noreturn sama5d27_first_stage(void *fdt)
+{
+	at91_wdt_disable(IOMEM(SAMA5D2_BASE_WDT));
+	sama5d2_pmc_init(MASTER_CLOCK);
+
+	ek_turn_led(RGB_LED_RED | RGB_LED_GREEN); /* Yellow */
+
+	if (IS_ENABLED(CONFIG_DEBUG_LL))
+		ek_dbgu_init();
+
+	sama5d2_ddr2_init(&ddramc_reg_config);
+
+	ek_turn_led(RGB_LED_GREEN);
+	barebox_arm_entry(SAMA5_DDRCS, SZ_128M, fdt);
+}
+
 extern char __dtb_z_at91_sama5d27_som1_ek_start[];
 
 ENTRY_FUNCTION(start_sama5d27_som1_ek, r0, r1, r2)
 {
-	void *fdt;
+	void *fdt = NULL;
 
 	arm_cpu_lowlevel_init();
 
 	arm_setup_stack(SAMA5D2_SRAM_BASE + SAMA5D2_SRAM_SIZE - 16);
 
+	if (IS_ENABLED(CONFIG_MACH_SAMA5D27_SOM1_DT))
+		fdt = __dtb_z_at91_sama5d27_som1_ek_start;
+
+	if (get_pc() < SAMA5_DDRCS) {
+		relocate_to_current_adr();
+		setup_c();
+		barrier();
+
+		sama5d27_first_stage(fdt);
+	}
+
+	if (fdt)
+		fdt += get_runtime_offset();
+
 	if (IS_ENABLED(CONFIG_DEBUG_LL))
 		ek_dbgu_init();
-
-	fdt = __dtb_z_at91_sama5d27_som1_ek_start + get_runtime_offset();
 
 	ek_turn_led(RGB_LED_GREEN);
 	barebox_arm_entry(SAMA5_DDRCS, SZ_128M, fdt);
