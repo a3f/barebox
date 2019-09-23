a3f/barebox

commit 46bdae84e8ac7ee1e7efa3e9b5dfedabb0de0bf9

dtc: Update update-dtc-source.sh from Linux
This updates update-dtc-source.sh from Linux-5.3-rc4.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

scripts/dtc/update-dtc-source.sh
@@ -4,14 +4,15 @@
 #
 # This script assumes that the dtc and the linux git trees are in the
 # same directory. After building dtc in the dtc directory, it copies the
-# source files into the scripts/dtc directory in barebox and creates a git
-# commit updating them to the new version.
+# source files and generated source file(s) into the scripts/dtc directory
+# in the kernel and creates a git commit updating them to the new
+# version.
 #
-# Usage: from the top level barebox source tree, run:
+# Usage: from the top level Linux source tree, run:
 # $ ./scripts/dtc/update-dtc-source.sh
 #
 # The script will change into the dtc tree, build and test dtc, copy the
-# relevant files into the barebox tree and create a git commit. The commit
+# relevant files into the kernel tree and create a git commit. The commit
 # message will need to be modified to reflect the version of DTC being
 # imported
 #
@@ -31,9 +32,9 @@ DTC_UPSTREAM_PATH=`pwd`/../dtc
 DTC_LINUX_PATH=`pwd`/scripts/dtc
 
 DTC_SOURCE="checks.c data.c dtc.c dtc.h flattree.c fstree.c livetree.c srcpos.c \
-		srcpos.h treesource.c util.c util.h version_gen.h Makefile.dtc \
-		dtc-lexer.l dtc-parser.y fdtget.c"
-LIBFDT_SOURCE="fdt.c fdt.h fdt_addresses.c fdt_empty_tree.c \
+		srcpos.h treesource.c util.c util.h version_gen.h yamltree.c Makefile.dtc \
+		dtc-lexer.l dtc-parser.y"
+LIBFDT_SOURCE="Makefile.libfdt fdt.c fdt.h fdt_addresses.c fdt_empty_tree.c \
 		fdt_overlay.c fdt_ro.c fdt_rw.c fdt_strerror.c fdt_sw.c \
 		fdt_wip.c libfdt.h libfdt_env.h libfdt_internal.h"
 
@@ -58,13 +59,13 @@ for f in $DTC_SOURCE; do
 	git add ${f}
 done
 for f in $LIBFDT_SOURCE; do
-       cp ${DTC_UPSTREAM_PATH}/libfdt/${f} ${f}
-       git add ${f}
+       cp ${DTC_UPSTREAM_PATH}/libfdt/${f} libfdt/${f}
+       git add libfdt/${f}
 done
 
-sed -i -- 's/#include <libfdt_env.h>/#include "libfdt_env.h"/g' ./libfdt.h
-sed -i -- 's/#include <fdt.h>/#include "fdt.h"/g' ./libfdt.h
-git add ./libfdt.h
+sed -i -- 's/#include <libfdt_env.h>/#include "libfdt_env.h"/g' ./libfdt/libfdt.h
+sed -i -- 's/#include <fdt.h>/#include "fdt.h"/g' ./libfdt/libfdt.h
+git add ./libfdt/libfdt.h
 
 commit_msg=$(cat << EOF
 scripts/dtc: Update to upstream version ${dtc_version}

commit 04650e95aeef0d9f5f48b765e14903a32265cbf3

scripts/dtc: Reinstate barebox-specifics in update-dtc-source.sh
The barebox update-dtc-source.sh differs from the Linux one in that it
installs fdtget as well, because it's required for barebox IMD to work.
Readd fdtget.c to DTC_SOURCE and while at it reinstate the old comments.

Fixes: 46bdae84e8 ("dtc: Update update-dtc-source.sh from Linux")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

scripts/dtc/update-dtc-source.sh
@@ -4,15 +4,14 @@
 #
 # This script assumes that the dtc and the linux git trees are in the
 # same directory. After building dtc in the dtc directory, it copies the
-# source files and generated source file(s) into the scripts/dtc directory
-# in the kernel and creates a git commit updating them to the new
-# version.
+# source files into the scripts/dtc directory in barebox and creates a git
+# commit updating them to the new version.
 #
-# Usage: from the top level Linux source tree, run:
+# Usage: from the top level barebox source tree, run:
 # $ ./scripts/dtc/update-dtc-source.sh
 #
 # The script will change into the dtc tree, build and test dtc, copy the
-# relevant files into the kernel tree and create a git commit. The commit
+# relevant files into the barebox tree and create a git commit. The commit
 # message will need to be modified to reflect the version of DTC being
 # imported
 #
@@ -33,7 +32,7 @@ DTC_LINUX_PATH=`pwd`/scripts/dtc
 
 DTC_SOURCE="checks.c data.c dtc.c dtc.h flattree.c fstree.c livetree.c srcpos.c \
 		srcpos.h treesource.c util.c util.h version_gen.h yamltree.c Makefile.dtc \
-		dtc-lexer.l dtc-parser.y"
+		dtc-lexer.l dtc-parser.y fdtget.c"
 LIBFDT_SOURCE="Makefile.libfdt fdt.c fdt.h fdt_addresses.c fdt_empty_tree.c \
 		fdt_overlay.c fdt_ro.c fdt_rw.c fdt_strerror.c fdt_sw.c \
 		fdt_wip.c libfdt.h libfdt_env.h libfdt_internal.h"