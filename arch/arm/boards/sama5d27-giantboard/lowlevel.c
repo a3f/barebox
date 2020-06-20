1c8af4baddf24ff2cfb07a544629eb0ec5c299cc
Ahmad Fatoum <ahmad@a3f.at>
2020-06-20 21:22:48 +0200

    ARM: at91: sama5d27-giantboard: add additional first stage entry point
    The BootROM constrains us to a 64K big first stage bootloader. Add a PBL
    entry point for a xload barebox that sets up the minimum necessary to
    load a FAT32 barebox.bin from the SD-Card.
    
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>

--- arch/arm/boards/sama5d27-giantboard/lowlevel.c
+++ arch/arm/boards/sama5d27-giantboard/lowlevel.c
@@ -5,13 +5,12 @@
 
 #include <common.h>
 #include <init.h>
-
-#include <asm/barebox-arm-head.h>
 #include <mach/barebox-arm.h>
 #include <mach/sama5d2_ll.h>
+#include <mach/xload.h>
+#include <mach/sama5d2-sip-ddramc.h>
 #include <mach/iomux.h>
 #include <debug_ll.h>
-#include <mach/at91_dbgu.h>
 
 /* PCK = 492MHz, MCK = 164MHz */
 #define MASTER_CLOCK	164000000
@@ -20,8 +19,24 @@ static void dbgu_init(void)
 {
 	sama5d2_dbgu_setup_ll(SAMA5D2_ID_UART1, AT91_PIN_PD3, AT91_MUX_PERIPH_A,
 			      MASTER_CLOCK, 115200);
+}
 
-	putc_ll('>');
+SAMA5_ENTRY_FUNCTION(start_sama5d27_giantboard_xload_mmc, r4)
+{
+	sama5d2_lowlevel_init();
+
+	dbgu_init();
+	if (IS_ENABLED(CONFIG_DEBUG_LL))
+		putc_ll('>');
+
+	relocate_to_current_adr();
+	setup_c();
+
+	pbl_set_putc(at91_dbgu_putc, SAMA5D2_BASE_UART1);
+
+	sama5d2_udelay_init(MASTER_CLOCK);
+	sama5d2_d1g_ddrconf();
+	sama5d2_sdhci_start_image(r4);
 }
 
 extern char __dtb_z_at91_sama5d27_giantboard_start[];
@@ -30,10 +45,8 @@ SAMA5_ENTRY_FUNCTION(start_sama5d27_giantboard, r4)
 {
 	void *fdt;
 
-	arm_cpu_lowlevel_init();
-
 	if (IS_ENABLED(CONFIG_DEBUG_LL))
-		dbgu_init();
+		putc_ll('>');
 
 	fdt = __dtb_z_at91_sama5d27_giantboard_start + get_runtime_offset();
 
