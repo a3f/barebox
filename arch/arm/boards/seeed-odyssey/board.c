4e7dc436d42becb88d00278f5c5e6e58eea803d0
Ahmad Fatoum <ahmad@a3f.at>
2020-07-28 22:11:57 +0200

    [WIP] ARM: stm32mp: add support for Seeed Odyssey board
    Board consists of SoM with stm32mp157c with 4G eMMC and 512M DDR3 RAM.
    Carrier board features USB and ETH interfaces, SD card connector,
    wifi and BT chip AP6236.
    
    [afa: untested]
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>

---
+++ arch/arm/boards/seeed-odyssey/board.c
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0+
+#include <common.h>
+#include <linux/sizes.h>
+#include <init.h>
+#include <asm/memory.h>
+#include <mach/bbu.h>
+#include <bootsource.h>
+#include <of.h>
+
+static int odyssey_device_init(void)
+{
+	int flags;
+	if (!of_machine_is_compatible("seeed,stm32mp157c-odyssey-som"))
+		return 0;
+
+	flags = bootsource_get_instance() == 0 ? BBU_HANDLER_FLAG_DEFAULT : 0;
+	stm32mp_bbu_mmc_register_handler("sd", "/dev/mmc0.ssbl", flags);
+
+	flags = bootsource_get_instance() == 1 ? BBU_HANDLER_FLAG_DEFAULT : 0;
+	stm32mp_bbu_mmc_register_handler("emmc", "/dev/mmc1.ssbl", flags);
+
+
+	if (bootsource_get_instance() == 0)
+		of_device_enable_path("/chosen/environment-sd");
+	else
+		of_device_enable_path("/chosen/environment-emmc");
+
+	return 0;
+}
+device_initcall(odyssey_device_init);
