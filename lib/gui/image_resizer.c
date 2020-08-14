a3c6226eca502c3025805113cc9e5c022ea344ec
Ahmad Fatoum <ahmad@a3f.at>
2020-08-14 14:34:52 +0200

    fixup! gui: add image resizing support

--- lib/gui/image_resizer.c
+++ lib/gui/image_resizer.c
@@ -4,7 +4,7 @@
 #include <common.h>
 #include <gui/graphic_utils.h>
 #include <linux/bitfield.h>
-#include "fixed.h"
+#include "real.h"
 
 #define RED_OFFSET	16
 #define GREEN_OFFSET	8
@@ -36,7 +36,7 @@ int gu_screen_blit_resized(struct screen *sc,
 			   int orig_width, int orig_height)
 {
 	struct fb_info *info = sc->info;
-	fixed_t width_scale, height_scale;
+	real_t width_scale, height_scale;
 	int x, y;
 
 	pr_debug("Resizing from %d x %d -> %d x %d\n",
@@ -48,105 +48,107 @@ int gu_screen_blit_resized(struct screen *sc,
 		return 0;
 	}
 
-	width_scale = fixed_div(int_to_fixed(orig_width), int_to_fixed(info->xres));
-	height_scale = fixed_div(int_to_fixed(orig_height), int_to_fixed(info->yres));
+	width_scale = real_div(int_to_real(orig_width), int_to_real(info->xres));
+	height_scale = real_div(int_to_real(orig_height), int_to_real(info->yres));
 
 	for (y = 0; y < info->yres; y++) {
-		fixed_t sy1, sy2;
+		real_t sy1, sy2;
 
-		sy1 = fixed_mul(int_to_fixed(y), height_scale);
-		sy2 = fixed_mul(int_to_fixed(y + 1), height_scale);
+		sy1 = real_mul(int_to_real(y), height_scale);
+		sy2 = real_mul(int_to_real(y + 1), height_scale);
 
 		for (x = 0; x < info->xres; x++) {
-			fixed_t sx1, sx2;
-			fixed_t sx, sy;
-			fixed_t spixels = 0;
-			fixed_t red = 0, green = 0, blue = 0;
+			real_t sx1, sx2;
+			real_t sx, sy;
+			real_t spixels = 0;
+			real_t red = 0, green = 0, blue = 0;
 
-			sx1 = fixed_mul(int_to_fixed(x), width_scale);
-			sx2 = fixed_mul(int_to_fixed(x + 1), width_scale);
+			sx1 = real_mul(int_to_real(x), width_scale);
+			sx2 = real_mul(int_to_real(x + 1), width_scale);
 			sy = sy1;
 
 			do {
-				fixed_t yportion;
+				real_t yportion;
 
-				if (fixed_floor(sy) == fixed_floor(sy1)) {
-					yportion = FIXED_1 - (sy - fixed_floor(sy));
+				if (real_floor(sy) == real_floor(sy1)) {
+					yportion = REAL_C(1) - (sy - real_floor(sy));
 					if (yportion > sy2 - sy1) {
 						yportion = sy2 - sy1;
 					}
-					sy = fixed_floor(sy);
+					sy = real_floor(sy);
 				}
-				else if (sy == fixed_floor(sy2)) {
-					yportion = sy2 - fixed_floor(sy2);
+				else if (sy == real_floor(sy2)) {
+					yportion = sy2 - real_floor(sy2);
 				}
 				else {
-					yportion = FIXED_1;
+					yportion = REAL_C(1);
 				}
 
 				sx = sx1;
 
 				do {
 					u8 r, g, b;
-					fixed_t xportion;
-					fixed_t pcontribution;
+					real_t xportion;
+					real_t pcontribution;
 
-					if (fixed_floor(sx) == fixed_floor(sx1)) {
-						xportion = FIXED_1 - (sx - fixed_floor(sx));
+					if (real_floor(sx) == real_floor(sx1)) {
+						xportion = REAL_C(1) - (sx - real_floor(sx));
 						if (xportion > sx2 - sx1)	{
 							xportion = sx2 - sx1;
 						}
-						sx = fixed_floor(sx);
+						sx = real_floor(sx);
 					}
-					else if (sx == fixed_floor(sx2)) {
-						xportion = sx2 - fixed_floor(sx2);
+					else if (sx == real_floor(sx2)) {
+						xportion = sx2 - real_floor(sx2);
 					}
 					else {
-						xportion = FIXED_1;
+						xportion = REAL_C(1);
 					}
 
-					pcontribution = fixed_mul(xportion, yportion);
+					pcontribution = real_mul(xportion, yportion);
 
-					get_pixel(info, fixed_to_int(sx), fixed_to_int(sy),
+					get_pixel(info, real_to_int(sx), real_to_int(sy),
 						  &r, &g, &b);
 
-					red   += fixed_mul(int_to_fixed(r), pcontribution);
-					green += fixed_mul(int_to_fixed(g), pcontribution);
-					blue  += fixed_mul(int_to_fixed(b), pcontribution);
+					red   += real_mul(int_to_real(r), pcontribution);
+					green += real_mul(int_to_real(g), pcontribution);
+					blue  += real_mul(int_to_real(b), pcontribution);
 
 					spixels += pcontribution;
-					sx += FIXED_1;
+					sx += REAL_C(1);
 				} while (sx < sx2);
 
-				sy += FIXED_1;
+				sy += REAL_C(1);
 			} while (sy < sy2);
 
+#if 0
 			// If rgba get too large for the fixed-point representation, fallback to the floating point routine
 			// This should only happen with very large images
 			if (red < 0 || green < 0 || blue < 0) {
 				pr_warn("fixed-point overflow: %d %d %d\n", red, green, blue);
 				return -EDOM;
 			}
+#endif
 
 			if (spixels != 0) {
-				spixels = fixed_div(FIXED_1, spixels);
+				spixels = real_div(REAL_C(1), spixels);
 
-				red	= fixed_mul(red, spixels);
-				green	= fixed_mul(green, spixels);
-				blue	= fixed_mul(blue, spixels);
+				red	= real_mul(red, spixels);
+				green	= real_mul(green, spixels);
+				blue	= real_mul(blue, spixels);
 			}
 
 			/* Clamping to allow for rounding errors above */
-			red = min(red, FIXED_255);
-			green = min(green, FIXED_255);
-			blue = min(blue, FIXED_255);
+			red = min(red, REAL_C(255));
+			green = min(green, REAL_C(255));
+			blue = min(blue, REAL_C(255));
 
 			pr_debug("  -> @(%d, %d) = %x (%d,%d,%d)\n",
-				 x, y, COL(fixed_to_int(red), fixed_to_int(green), fixed_to_int(blue)),
-				 fixed_to_int(red), fixed_to_int(green), fixed_to_int(blue));
+				 x, y, COL(real_to_int(red), real_to_int(green), real_to_int(blue)),
+				 real_to_int(red), real_to_int(green), real_to_int(blue));
 
 			put_rgb_pixel(info, x, y,
-				fixed_to_int(red), fixed_to_int(green), fixed_to_int(blue));
+				real_round(red), real_round(green), real_round(blue));
 		}
 	}
 
