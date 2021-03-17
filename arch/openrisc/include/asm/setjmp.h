7fc56f27548294e4682088054d2bfb9aef69c072
Ahmad Fatoum <ahmad@a3f.at>
2021-03-17 11:31:45 +0100

    fixup! openrisc: Implement setjmp/longjmp/initjmp
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>

--- arch/openrisc/include/asm/setjmp.h
+++ arch/openrisc/include/asm/setjmp.h
@@ -12,6 +12,6 @@ typedef long int jmp_buf[13];
 
 int setjmp(jmp_buf jmp) __attribute__((returns_twice));
 void longjmp(jmp_buf jmp, int ret) __attribute__((noreturn));
-int initjmp(jmp_buf jmp, void __noreturn (*func)(void), void *stack_top);
+int initjmp(jmp_buf jmp, void __attribute__((noreturn)) (*func)(void), void *stack_top);
 
 #endif /* _OR1K_BITS_SETJMP_H */
