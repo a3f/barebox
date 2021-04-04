738c73bcaba61ee023366cee42d9292956302681
Ahmad Fatoum <ahmad@a3f.at>
2021-04-04 08:46:56 +0200

    MIPS: qemu-malta: replace board code with syscon-reboot
    We got two identical implementations of system controller reset,
    one in mach-malta and another in mach-bcm47xx.
    
    For the Malta board, Linux represents this as syscon-reboot nodes,
    do likewise. This could probably done for the dlink-dir-320, but
    would increase code size. This is not done here, because inability to
    test.
    
    Tested on Qemu Malta board.
    
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>

--- arch/mips/mach-malta/include/mach/hardware.h
+++ arch/mips/mach-malta/include/mach/hardware.h
@@ -11,10 +11,4 @@
 #define MALTA_CBUS_UART	0xbf000900
 #define MALTA_CBUS_UART_SHIFT	3
 
-/*
- * Reset register.
- */
-#define SOFTRES_REG	0xbf000500
-#define GORESET		0x42
-
 #endif  /* __INCLUDE_ARCH_HARDWARE_H__ */
