8bffd695e7445f85bf43122ccc1353bdd6400f50
Ahmad Fatoum <ahmad@a3f.at>
2021-12-23 13:39:58 +0100

    crypto: add RC4 implementation for rockchip image handling
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>

---
+++ include/crypto/rc4.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __INCLUDE_CRYPTO_RC4_H
+#define __INCLUDE_CRYPTO_RC4_H
+
+void rc4_encode(unsigned char *buf, unsigned int len, unsigned char key[16]);
+
+#endif
