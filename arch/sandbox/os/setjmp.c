98e30ff54467cc5ef40ab32604e195e4fb512a7f
Ahmad Fatoum <ahmad@a3f.at>
2023-10-03 21:26:10 +0200

    sandbox: asm: switch to using makecontext
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>

--- arch/sandbox/os/setjmp.c
+++ arch/sandbox/os/setjmp.c
@@ -15,8 +15,9 @@
 #undef _FORTIFY_SOURCE
 #endif
 
-#include <pthread.h>
+#include <ucontext.h>
 #include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <setjmp.h>
 #include <signal.h>
@@ -24,146 +25,89 @@
 typedef sigjmp_buf _jmp_buf __attribute__((aligned((16))));
 _Static_assert(sizeof(_jmp_buf) <= 512, "sigjmp_buf size exceeds expectation");
 
-/*
- * Information for the signal handler (trampoline)
- */
-static struct {
-	_jmp_buf *reenter;
-	void (*entry)(void);
-	volatile sig_atomic_t called;
-} tr_state;
+static _jmp_buf setup;
 
 /*
- * "boot" function
- * This is what starts the coroutine, is called from the trampoline
- * (from the signal handler when it is not signal handling, read ahead
- * for more information).
+ * Information for the trampoline
  */
-static void __attribute__((noinline, noreturn))
-coroutine_bootstrap(void (*entry)(void))
-{
-	for (;;)
-		entry();
-}
+struct tr_state {
+	_jmp_buf *reenter;
+	void (*entry)(void);
+};
+
+/*
+ * va_args to makecontext() must be type 'int', so passing
+ * the pointer we need may require several int args. This
+ * union is a quick hack to let us do that
+ */
+union cc_arg {
+    struct tr_state tr_state;
+    int i[4];
+};
 
 /*
  * This is used as the signal handler. This is called with the brand new stack
  * (thanks to sigaltstack). We have to return, given that this is a signal
  * handler and the sigmask and some other things are changed.
  */
-static void coroutine_trampoline(int signal)
+static void coroutine_trampoline(int i0, int i1, int i2, int i3)
 {
-	/* Get the thread specific information */
-	tr_state.called = 1;
+	union cc_arg arg = { .i = { i0, i1, i2, i3 } };
 
-	/*
-	 * Here we have to do a bit of a ping pong between the caller, given that
-	 * this is a signal handler and we have to do a return "soon". Then the
-	 * caller can reestablish everything and do a siglongjmp here again.
-	 */
-	if (!sigsetjmp(*tr_state.reenter, 0)) {
-		return;
+	printf("%s:%d prep setjmp2\n", __func__, __LINE__);
+
+	if (!sigsetjmp(*arg.tr_state.reenter, 0)) {
+		printf("%s:%d co longjmp: %p\n", __func__, __LINE__,
+		       setup);
+		siglongjmp(setup, 1);
 	}
 
-	/*
-	 * Ok, the caller has siglongjmp'ed back to us, so now prepare
-	 * us for the real machine state switching. We have to jump
-	 * into another function here to get a new stack context for
-	 * the auto variables (which have to be auto-variables
-	 * because the start of the thread happens later). Else with
-	 * PIC (i.e. Position Independent Code which is used when PTH
-	 * is built as a shared library) most platforms would
-	 * horrible core dump as experience showed.
-	 */
-	coroutine_bootstrap(tr_state.entry);
+	printf("%s:%d entry %p\n", __func__, __LINE__, arg.tr_state.entry);
+
+	for (;;)
+		arg.tr_state.entry();
 }
 
 int initjmp(_jmp_buf jmp, void (*func)(void), void *stack_top)
 {
-	struct sigaction sa;
-	struct sigaction osa;
-	stack_t ss;
-	stack_t oss;
-	sigset_t sigs;
-	sigset_t osigs;
 
-	/* The way to manipulate stack is with the sigaltstack function. We
-	 * prepare a stack, with it delivering a signal to ourselves and then
-	 * put sigsetjmp/siglongjmp where needed.
-	 * This has been done keeping coroutine-ucontext (from the QEMU project)
-	 * as a model and with the pth ideas (GNU Portable Threads).
-	 * See coroutine-ucontext for the basics of the coroutines and see
-	 * pth_mctx.c (from the pth project) for the
-	 * sigaltstack way of manipulating stacks.
+	ucontext_t old_uc, uc;
+	union cc_arg arg = {};
+
+	/* The ucontext functions preserve signal masks which incurs a
+	 * system call overhead.  sigsetjmp(buf, 0)/siglongjmp() does not
+	 * preserve signal masks but only works on the current stack.
+	 * Since we need a way to create and switch to a new stack, use
+	 * the ucontext functions for that but sigsetjmp()/siglongjmp() for
+	 * everything else.
 	 */
 
-	tr_state.entry = func;
-	tr_state.reenter = (void *)jmp;
+	if (getcontext(&uc) == -1)
+		return -errno;
 
-	/*
-	 * Preserve the SIGUSR2 signal state, block SIGUSR2,
-	 * and establish our signal handler. The signal will
-	 * later transfer control onto the signal stack.
-	 */
-	sigemptyset(&sigs);
-	sigaddset(&sigs, SIGUSR2);
-	pthread_sigmask(SIG_BLOCK, &sigs, &osigs);
-	sa.sa_handler = coroutine_trampoline;
-	sigfillset(&sa.sa_mask);
-	sa.sa_flags = SA_ONSTACK;
-	if (sigaction(SIGUSR2, &sa, &osa) != 0) {
-		return -1;
-	}
+	uc.uc_link = &old_uc;
 
 	/*
 	 * Set the new stack.
 	 */
-	ss.ss_sp = stack_top - CONFIG_STACK_SIZE;
-	ss.ss_size = CONFIG_STACK_SIZE;
-	ss.ss_flags = 0;
-	if (sigaltstack(&ss, &oss) < 0) {
-		return -1;
-	}
+	uc.uc_stack.ss_sp = stack_top - CONFIG_STACK_SIZE;
+	uc.uc_stack.ss_size = CONFIG_STACK_SIZE;
+	uc.uc_stack.ss_flags = 0;
+
+	arg.tr_state.reenter = (void *)jmp;
+	arg.tr_state.entry = func;
+
+	makecontext(&uc, (void (*)(void))coroutine_trampoline,
+		    4, arg.i[0], arg.i[1], arg.i[2], arg.i[3]);
+
+
+	/* swapcontext() in, siglongjmp() back out */
+	if (sigsetjmp(setup, 0) == 0)
+		swapcontext(&old_uc, &uc);
 
 	/*
-	 * Now transfer control onto the signal stack and set it up.
-	 * It will return immediately via "return" after the sigsetjmp()
-	 * was performed. Be careful here with race conditions.  The
-	 * signal can be delivered the first time sigsuspend() is
-	 * called.
-	 */
-	tr_state.called = 0;
-	pthread_kill(pthread_self(), SIGUSR2);
-	sigfillset(&sigs);
-	sigdelset(&sigs, SIGUSR2);
-	while (!tr_state.called) {
-		sigsuspend(&sigs);
-	}
-
-	/*
-	 * Inform the system that we are back off the signal stack by
-	 * removing the alternative signal stack. Be careful here: It
-	 * first has to be disabled, before it can be removed.
-	 */
-	sigaltstack(NULL, &ss);
-	ss.ss_flags = SS_DISABLE;
-	if (sigaltstack(&ss, NULL) < 0) {
-		return -1;
-	}
-	sigaltstack(NULL, &ss);
-	if (!(oss.ss_flags & SS_DISABLE)) {
-		sigaltstack(&oss, NULL);
-	}
-
-	/*
-	 * Restore the old SIGUSR2 signal handler and mask
-	 */
-	sigaction(SIGUSR2, &osa, NULL);
-	pthread_sigmask(SIG_SETMASK, &osigs, NULL);
-
-	/*
-	 * jmp can now be used to enter the trampoline again, but not as a
-	 * signal handler. Instead it's longjmp'd to directly.
+	 * jmp can now be used to enter the trampoline again, but without
+	 * having to involve swapcontext.
 	 */
 
 	return 0;
