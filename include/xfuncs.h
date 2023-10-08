798af25361fa30fe0b2131a9001edfd8b2cf42c2
Ahmad Fatoum <ahmad@a3f.at>
2023-10-08 15:50:36 +0200

    xfuncs: stub out API when built for PBL
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>

--- include/xfuncs.h
+++ include/xfuncs.h
@@ -4,9 +4,11 @@
 
 #include <linux/types.h>
 #include <linux/compiler.h>
+#include <asm-generic/bug.h>
 #include <stdarg.h>
 #include <wchar.h>
 
+#ifndef __PBL__
 void *xmalloc(size_t size) __xalloc_size(1);
 void *xrealloc(void *ptr, size_t size) __xrealloc_size(2);
 void *xzalloc(size_t size) __xalloc_size(1);
@@ -20,5 +22,22 @@ char *xvasprintf(const char *fmt, va_list ap) __returns_nonnull;
 wchar_t *xstrdup_wchar(const wchar_t *src);
 wchar_t *xstrdup_char_to_wchar(const char *src);
 char *xstrdup_wchar_to_char(const wchar_t *src);
+
+#else
+
+static inline void *xmalloc(size_t size) { BUG(); }
+static inline void *xrealloc(void *ptr, size_t size) { BUG(); }
+static inline void *xzalloc(size_t size) { BUG(); }
+static inline char *xstrdup(const char *s) { BUG(); }
+static inline char *xstrndup(const char *s, size_t size) { BUG(); }
+static inline void* xmemalign(size_t alignment, size_t bytes) { BUG(); }
+static inline void* xmemdup(const void *orig, size_t size) { BUG(); }
+static inline char *xasprintf(const char *fmt, ...) { BUG(); }
+static inline char *xvasprintf(const char *fmt, va_list ap) { BUG(); }
+
+static inline wchar_t *xstrdup_wchar(const wchar_t *src) { BUG(); }
+static inline wchar_t *xstrdup_char_to_wchar(const char *src) { BUG(); }
+static inline char *xstrdup_wchar_to_char(const wchar_t *src) { BUG(); }
+#endif
 
 #endif /* __XFUNCS_H */
