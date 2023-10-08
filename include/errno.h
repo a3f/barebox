8d7d75583bcffcb046880d8266650edbd04d83a5
Ahmad Fatoum <ahmad@a3f.at>
2023-10-08 15:49:34 +0200

    errno: stub out perror/strerror API when built for PBL
    To optimize out calls to undefined functions in PBL at compile-time
    instead of link time, let's define stubs for the functions that
    are referenced in obj-pbl-y files, but inside function sections
    that are ultimately unreferenced.
    
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>

--- include/errno.h
+++ include/errno.h
@@ -7,8 +7,18 @@
 
 extern int errno;
 
+#ifndef __PBL__
 void perror(const char *s);
 const char *strerror(int errnum);
+#else
+static inline void perror(const char *s)
+{
+}
+static inline const char *strerror(int errnum)
+{
+	return "unknown error";
+}
+#endif
 
 static inline int errno_set(int err)
 {
