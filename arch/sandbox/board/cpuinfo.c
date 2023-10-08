6df9792187bc26aa5551471bd9c5d4548150a2de
Ahmad Fatoum <ahmad@a3f.at>
2023-10-08 20:53:41 +0200

    sandbox: add cpuinfo command
    We have stack dump support in sandbox via AddressSanitizer, if it's
    compiled in. To make it easier to test proper operation, let's a cpuinfo
    command like we already do on ARM and give it the same -s option.
    
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>

---
+++ arch/sandbox/board/cpuinfo.c
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <common.h>
+#include <getopt.h>
+#include <command.h>
+#include <complete.h>
+
+static int do_cpuinfo(int argc, char *argv[])
+{
+	int opt;
+
+	while ((opt = getopt(argc, argv, "s")) > 0) {
+		switch (opt) {
+		case 's':
+			if (!IS_ENABLED(CONFIG_ARCH_HAS_STACK_DUMP))
+				return -ENOSYS;
+
+			dump_stack();
+			return 0;
+		default:
+			return COMMAND_ERROR_USAGE;
+		}
+	}
+
+	return 0;
+}
+
+BAREBOX_CMD_HELP_START(cpuinfo)
+BAREBOX_CMD_HELP_TEXT("Shows misc info about CPU")
+BAREBOX_CMD_HELP_OPT ("-s", "print call stack info (if supported)")
+BAREBOX_CMD_HELP_END
+
+BAREBOX_CMD_START(cpuinfo)
+	.cmd            = do_cpuinfo,
+	BAREBOX_CMD_DESC("show info about CPU")
+	BAREBOX_CMD_OPTS("[-s]")
+	BAREBOX_CMD_GROUP(CMD_GRP_INFO)
+	BAREBOX_CMD_COMPLETE(empty_complete)
+	BAREBOX_CMD_HELP(cmd_cpuinfo_help)
+BAREBOX_CMD_END
