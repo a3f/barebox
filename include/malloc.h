b6ce64452adc061e9f119b795ac65c0605d13ef9
Ahmad Fatoum <ahmad@a3f.at>
2023-10-12 19:25:59 +0200

    malloc: add PBL stubs
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>

--- include/malloc.h
+++ include/malloc.h
@@ -5,6 +5,7 @@
 #include <linux/compiler.h>
 #include <types.h>
 
+#ifndef __PBL__
 void *malloc(size_t) __alloc_size(1);
 void free(void *);
 void *realloc(void *, size_t) __realloc_size(2);
@@ -14,5 +15,38 @@ void malloc_stats(void);
 void *sbrk(ptrdiff_t increment);
 
 int mem_malloc_is_initialized(void);
+#else
+static inline void *malloc(size_t nbytes)
+{
+	return NULL;
+}
+static inline void free(void *buf)
+{
+}
+static inline void *realloc(void *orig, size_t nbytes)
+{
+	return NULL;
+}
+static inline void *memalign(size_t align, size_t size)
+{
+	return NULL;
+}
+static inline void *calloc(size_t num, size_t size)
+{
+	return NULL;
+}
+static inline void malloc_stats(void)
+{
+}
+static inline void *sbrk(ptrdiff_t increment)
+{
+	return NULL;
+}
+
+static inline int mem_malloc_is_initialized(void)
+{
+	return 0;
+}
+#endif
 
 #endif /* __MALLOC_H */
