a3f/barebox

commit 7876018f73330338c029fa0ec32e59367354a123

fs: implement unreaddir
When iterating over a directory, it can be useful to put back the just
read directory entry, so it can be retried at a later time. This will be
needed for the EFI loader variable support.

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20221010061437.2085412-4-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

fs/fs.c
@@ -890,6 +890,21 @@ static int fillonedir(struct dir_context *ctx, const char *name, int namlen,
 	return 0;
 }
 
+int unreaddir(DIR *dir, const struct dirent *d)
+{
+	struct readdir_entry *entry;
+
+	if (d != &dir->d)
+		return -EINVAL;
+
+	entry = xzalloc(sizeof(*entry));
+	entry->d = *d;
+	list_add_tail(&entry->list, &dir->entries);
+
+	return 0;
+}
+EXPORT_SYMBOL(unreaddir);
+
 struct dirent *readdir(DIR *dir)
 {
 	struct readdir_entry *entry;

include/dirent.h
@@ -18,6 +18,7 @@ typedef struct dir {
 
 DIR *opendir(const char *pathname);
 struct dirent *readdir(DIR *dir);
+int unreaddir(DIR *dir, const struct dirent *d);
 int closedir(DIR *dir);
 
 #endif /* __DIRENT_H */

test/self/ramfs.c
@@ -44,6 +44,7 @@ static inline int get_file_count(int i)
 
 static void test_ramfs(void)
 {
+	int files[] = { 1, 3, 5, 7, 11, 13, 17 };
 	char fname[128];
 	char *content = NULL;
 	char *oldpwd = NULL;
@@ -127,6 +128,7 @@ static void test_ramfs(void)
 		goto out;
 
 	i = 1;
+	j = 0;
 
 	while ((d = readdir(dir))) {
 		size_t size = 0;
@@ -150,7 +152,20 @@ static void test_ramfs(void)
 
 		free(buf);
 
-		i++;
+		/*
+		 * For select files, we test unreaddir once and check if
+		 * we read back same element on repeated readdir
+		 */
+		for (j = 0; j < ARRAY_SIZE(files); j++) {
+			if (size == files[j]) {
+				ret = unreaddir(dir, d);
+				expect_success(ret, "unreaddir");
+				files[j] = -1;
+				break;
+			}
+		}
+		if (j == ARRAY_SIZE(files))
+			i++;
 	}
 
 	expect_success(i == 90 ? 0 : -EINVAL,

commit 10fd98b8e89699e422c60f37e1aa2433b3cc06e4

fs: fix unreaddir, so readdir returns unread dirent first
unreaddir should return a directory entry at the top of the list, so
readdir consumes it again, not at the end.

Fixes: 7876018f7333 ("fs: implement unreaddir")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

fs/fs.c
@@ -1024,7 +1024,7 @@ int unreaddir(DIR *dir, const struct dirent *d)
 
 	entry = xzalloc(sizeof(*entry));
 	entry->d = *d;
-	list_add_tail(&entry->list, &dir->entries);
+	list_add(&entry->list, &dir->entries);
 
 	return 0;
 }