e13c73ff4b17ea657df9ac8bd8884b2f1c638ca0
Ahmad Fatoum <a.fatoum@pengutronix.de>
2024-05-17 07:56:12 +0200

    pinctrl: implement pinctrl_lookup_state/select_state
    Besides pinctrl_get_select, the Linux API provides separate functions to
    lookup a state and verify its existence and activating a state.
    
    To make porting code easier that calls the former during probe and the
    latter at runtime, define these functions and implement
    of_pinctrl_select_state on top of them.
    
    No functional change intended.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20240517055612.1685978-3-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/linux/pinctrl/consumer.h
+++ include/linux/pinctrl/consumer.h
@@ -3,6 +3,7 @@
 #define LINUX_PINCTRL_CONSUMER_H
 
 #include <linux/errno.h>
+#include <device.h>
 
 struct device;
 struct device_node;
@@ -10,6 +11,9 @@ struct device_node;
 struct pinctrl;
 
 #ifdef CONFIG_PINCTRL
+struct pinctrl_state *pinctrl_lookup_state(struct pinctrl *pinctrl,
+					   const char *name);
+int pinctrl_select_state(struct pinctrl *pinctrl, struct pinctrl_state *state);
 struct pinctrl *pinctrl_get_select(struct device *dev, const char *state);
 int pinctrl_select_state_default(struct device *dev);
 int of_pinctrl_select_state(struct device_node *np, const char *state);
@@ -19,6 +23,15 @@ int pinctrl_gpio_direction_output(unsigned int pin);
 int pinctrl_gpio_get_direction(unsigned pin);
 int pinctrl_single_probe(struct device *dev);
 #else
+static inline struct pinctrl_state *pinctrl_lookup_state(struct pinctrl *pinctrl,
+							 const char *name);
+{
+        return NULL;
+}
+static inline int pinctrl_select_state(struct pinctrl *pinctrl, struct pinctrl_state *state)
+{
+	return 0;
+}
 static inline struct pinctrl *pinctrl_get_select(struct device *dev, const char *state)
 {
 	return ERR_PTR(-ENODEV);
@@ -61,5 +74,15 @@ static inline int pinctrl_single_probe(struct device *dev)
 #endif
 
 static inline void pinctrl_put(struct pinctrl *pinctrl) {}
+
+static inline struct pinctrl *of_pinctrl_get(struct device_node *np)
+{
+	return (struct pinctrl *)np;
+}
+
+static inline struct pinctrl *pinctrl_get(struct device *dev)
+{
+	return of_pinctrl_get(dev_of_node(dev));
+}
 
 #endif /* LINUX_PINCTRL_CONSUMER_H */
