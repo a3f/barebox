3dde8e7dcdf0297e1378c936a347666c511f1f51
Ahmad Fatoum <ahmad@a3f.at>
2024-05-21 20:48:55 +0200

    fixup! kbuild: add support for merged defconfigs
    scripts: list-defconfigs.sh: remove interleaves new lines from output
    
    Github actions apparently chokes on new lines in the middle of the JSON,
    so let's avoid them.
    
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>

--- scripts/list-defconfigs.sh
+++ scripts/list-defconfigs.sh
@@ -4,12 +4,13 @@
 # Output json formatted defconfig list for Github Action consumption
 ARCH=${@:-$(for a in arch/*/; do basename $a; done)}
 
-echo '{ "include" : [ '
+# Github chokes on interleaved new lines, thus use printf
+printf '{ "include" : [ '
 for arch in $ARCH; do
 	make ARCH=$arch CROSS_COMPILE= help | \
 		awk '/_defconfig/ { print $1  }' | \
 		xargs -i printf '{ "arch": "%s", "config": "%s" }\n' \
 		"$arch" "{}" | \
 		paste -sd ',' -
-done | paste -sd ',' -
+done | paste -sd ',' - | tr -d '\n'
 echo '] }'
