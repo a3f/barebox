20789a2eb45c24476debe49f44b7408c348bb152
Ahmad Fatoum <ahmad@a3f.at>
2025-01-07 23:15:59 +0100

    libfile: give copy_file a flags parameter
    In preparation for adding an additional flag input to copy_file, let's
    turn the current copy_file verbose parameter in a bitmask.
    
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>

--- common/fastboot.c
+++ common/fastboot.c
@@ -744,7 +744,7 @@ static void cb_flash(struct fastboot *fb, const char *cmd)
 	}
 
 copy:
-	ret = copy_file(fb->tempname, filename, 1);
+	ret = copy_file(fb->tempname, filename, COPY_FILE_VERBOSE);
 	if (ret)
 		fastboot_tx_print(fb, FASTBOOT_MSG_FAIL,
 				  "write partition: %s", strerror(-ret));
