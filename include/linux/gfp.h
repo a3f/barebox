2d9ebaef3325859e8db9b2e1869e9ddc1dabc3b5
Ahmad Fatoum <ahmad@a3f.at>
2025-01-09 00:00:44 +0100

    include: gfp: define __GFP_BITS_MASK
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>

--- include/linux/gfp.h
+++ include/linux/gfp.h
@@ -9,4 +9,10 @@
 #define GFP_USER	0
 #define __GFP_NOWARN	0
 
+#define ___GFP_LAST_BIT 0
+
+/* Room for N __GFP_FOO bits */
+#define __GFP_BITS_SHIFT ___GFP_LAST_BIT
+#define __GFP_BITS_MASK ((__force gfp_t)((1 << __GFP_BITS_SHIFT) - 1))
+
 #endif
