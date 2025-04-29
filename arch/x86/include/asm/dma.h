48969817eb51b165d71b76eb2d7e57ba2c27e765
Ahmad Fatoum <a.fatoum@pengutronix.de>
2025-04-29 16:34:00 +0200

    x86: switch to new ARCH_DMA_FULLY_COHERENT
    By selecting ARCH_DMA_FULLY_COHERENT, we can make use of the new common
    code, instead of duplicating it in the header.
    
    This introduces two differences:
    
     - DMA coherent memory is now 32-byte aligned (defualt DMA_ALIGNMENT)
       instead of 4K alignment. The old alignment is excessive for a cache
       coherent system.
    
     - We return NULL instead of panic on allocation failure
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

--- arch/x86/include/asm/dma.h
+++ arch/x86/include/asm/dma.h
@@ -4,48 +4,4 @@
 #ifndef __ASM_DMA_H
 #define __ASM_DMA_H
 
-#include <linux/string.h>
-#include <linux/compiler.h>
-#include <xfuncs.h>
-#include <malloc.h>
-
-struct device;
-
-/*
- * x86 is cache coherent, so we need not do anything special here
- */
-
-#define dma_alloc_coherent dma_alloc_coherent
-static inline void *dma_alloc_coherent(struct device *dev,
-				       size_t size, dma_addr_t *dma_handle)
-{
-	void *ret = xmemalign(4096, size);
-	if (dma_handle)
-		*dma_handle = (dma_addr_t)ret;
-
-	memset(ret, 0, size);
-
-	return ret;
-}
-
-#define dma_free_coherent dma_free_coherent
-static inline void dma_free_coherent(struct device *dev,
-				     void *mem, dma_addr_t dma_handle,
-				     size_t size)
-{
-	free(mem);
-}
-
-#define arch_sync_dma_for_cpu arch_sync_dma_for_cpu
-static inline void arch_sync_dma_for_cpu(void *vaddr, size_t size,
-					 enum dma_data_direction dir)
-{
-}
-
-#define arch_sync_dma_for_device arch_sync_dma_for_device
-static inline void arch_sync_dma_for_device(void *vaddr, size_t size,
-					    enum dma_data_direction dir)
-{
-}
-
 #endif /* __ASM_DMA_H */
