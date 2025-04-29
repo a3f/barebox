5ca8c308a576294a48da730dc537e7852584c3cd
Ahmad Fatoum <a.fatoum@pengutronix.de>
2025-04-29 18:32:40 +0200

    ARM: nommu: switch to ARCH_DMA_FULLY_COHERENT
    The dummy implementation for nommu is basically the same as the generic
    implementation for cache-coherent systems (no cache = nothing to keep
    coherent), except for alignment, which would be 64 bytes instead of 4K
    as before.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

--- arch/arm/include/asm/dma.h
+++ arch/arm/include/asm/dma.h
@@ -1,59 +1,16 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /* SPDX-FileCopyrightText: 2012 Marc Kleine-Budde <mkl@pengutronix.de> */
 
-#include <linux/types.h>
-#include <linux/string.h>
-#include <xfuncs.h>
-#include <malloc.h>
+#ifndef __ASM_DMA_H
+#define __ASM_DMA_H
 
-#define DMA_ALIGNMENT	64
+#include <linux/pagemap.h>
 
-struct device;
+/* Maximum cache line size that we support */
+#define DMA_ALIGNMENT		64
+/* Coherent allocations are realized by marking pages uncached.
+ * For nommu, we want to keep the same alignment for symmetry.
+ */
+#define DMA_COHERENT_ALIGNMENT	PAGE_SIZE
 
-#if !defined(CONFIG_MMU) && !defined(CONFIG_ARMV7R_MPU)
-
-#define dma_alloc_coherent dma_alloc_coherent
-static inline void *dma_alloc_coherent(struct device *dev,
-				       size_t size, dma_addr_t *dma_handle)
-{
-	void *ret;
-
-	ret = memalign(4096, size);
-	if (!ret)
-		return NULL;
-
-	if (dma_handle)
-		*dma_handle = (dma_addr_t)ret;
-
-	memset(ret, 0, size);
-
-	return ret;
-}
-
-#define dma_alloc_writecombine dma_alloc_writecombine
-static inline void *dma_alloc_writecombine(struct device *dev,
-					   size_t size, dma_addr_t *dma_handle)
-{
-	return dma_alloc_coherent(dev, size, dma_handle);
-}
-
-#define dma_free_coherent dma_free_coherent
-static inline void dma_free_coherent(struct device *dev,
-				     void *mem, dma_addr_t dma_handle,
-				     size_t size)
-{
-	free(mem);
-}
-
-#define arch_sync_dma_for_cpu arch_sync_dma_for_cpu
-static inline void arch_sync_dma_for_cpu(void *vaddr, size_t size,
-					 enum dma_data_direction dir)
-{
-}
-
-#define arch_sync_dma_for_device arch_sync_dma_for_device
-static inline void arch_sync_dma_for_device(void *vaddr, size_t size,
-					    enum dma_data_direction dir)
-{
-}
-#endif
+#endif /* __ASM_DMA_H */
