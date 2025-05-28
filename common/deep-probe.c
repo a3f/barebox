45394e9eb3777711ee029a6bcae20da2ff8f5b91
Ahmad Fatoum <a.fatoum@barebox.org>
2025-05-28 14:28:20 +0200

    common: deep-probe: support specifying support in DT
    New boards should be deep probe enabled by default, which is currently
    done via board code. We want to support second stage boot on new boards
    without board code however, which means we need the possibility to make
    deep probe opt-out instead of opt-in.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>

--- common/deep-probe.c
+++ common/deep-probe.c
@@ -20,10 +20,17 @@ static enum deep_probe_state boardstate = DEEP_PROBE_UNKNOWN;
 
 bool deep_probe_is_supported(void)
 {
+	bool deep_probe_default = IS_ENABLED(CONFIG_DEEP_PROBE_DEFAULT);
 	struct deep_probe_entry *board;
+	struct device_node *root;
 
 	if (boardstate > DEEP_PROBE_UNKNOWN)
 		return boardstate;
+
+	/* deep probe requires resources to be described in DT */
+	root = of_get_root_node();
+	if (!root)
+		return false;
 
 	/* determine boardstate */
 	for (board = __barebox_deep_probe_start;
@@ -39,8 +46,21 @@ bool deep_probe_is_supported(void)
 		}
 	}
 
-	boardstate = DEEP_PROBE_NOT_SUPPORTED;
-	pr_info("not activated\n");
-	return false;
+	if (of_property_read_bool(root, "barebox,disable-deep-probe")) {
+		boardstate = DEEP_PROBE_NOT_SUPPORTED;
+		pr_info("disabled in device tree\n");
+	} else if (of_property_read_bool(root, "barebox,deep-probe")) {
+		boardstate = DEEP_PROBE_SUPPORTED;
+		pr_debug("enabled in device tree\n");
+	} else if (deep_probe_default) {
+		boardstate = DEEP_PROBE_SUPPORTED;
+		pr_debug("activated by default\n");
+	} else {
+		boardstate = DEEP_PROBE_NOT_SUPPORTED;
+		pr_warn("DT missing barebox,deep-probe or barebox,disable-deep-probe property\n");
+		pr_info("not activated by default\n");
+	}
+
+	return boardstate;
 }
 EXPORT_SYMBOL_GPL(deep_probe_is_supported);
