a3f/barebox

commit cf7b19df6541a89ad9b421e9369bc19707cd0235

fs: ramfs: do not bother unmounting ramfs on shutdown
It's pointless to free memory when we are about to boot a kernel that
will reclaim all memory anyway.

As ramfs only consists of such memory buffers, let's just skip its
freeing.

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20250605112618.1972479-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

fs/fs.c
@@ -835,6 +835,8 @@ static void fs_remove(struct device *dev)
 	int ret;
 
 	if (fsdev->dev.driver) {
+		if (!dev->driver->remove)
+			return;
 		dev->driver->remove(dev);
 		list_del(&fsdev->list);
 	}

fs/ramfs.c
@@ -435,10 +435,6 @@ static int ramfs_probe(struct device *dev)
 	return 0;
 }
 
-static void ramfs_remove(struct device *dev)
-{
-}
-
 static struct fs_driver ramfs_driver = {
 	.read      = ramfs_read,
 	.write     = ramfs_write,
@@ -446,7 +442,6 @@ static struct fs_driver ramfs_driver = {
 	.truncate  = ramfs_truncate,
 	.drv = {
 		.probe  = ramfs_probe,
-		.remove = ramfs_remove,
 		.name = "ramfs",
 	}
 };

commit 1f1e1f693c930f12e3a64bed163974efcc913c83

fs: do not skip fs_remove when calling umount ramfs
There's usually only a single ramfs mounted at / and given that the OS
will reclaim the memory anyway, we skip removal of ramfs.

Should further ramfs be mounted though, e.g. by tests, we run into a
use after free: fs_remove is skipped, but the unmount deallocates
the fs_device.

Fix this by only skipping removal for mounted file systems without a
remove callback when barebox_shutdown has been called.

Fixes: cf7b19df6541 ("fs: ramfs: do not bother unmounting ramfs on shutdown")
Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>

fs/fs.c
@@ -835,9 +835,11 @@ static void fs_remove(struct device *dev)
 	int ret;
 
 	if (fsdev->dev.driver) {
-		if (!dev->driver->remove)
+		if (barebox_system_state == BAREBOX_EXITING
+		    && !dev->driver->remove)
 			return;
-		dev->driver->remove(dev);
+		if (dev->driver->remove)
+			dev->driver->remove(dev);
 		list_del(&fsdev->list);
 	}
 