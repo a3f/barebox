a3f/barebox

commit 5b175c52b53b716e2b47d91044c18209baa4522a

commands: stat: add option for statat
The stat command is a debugging aid for developing the VFS, so it makes
sense to add support for dirfd, so they can be tested interactively.

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20240304190038.3486881-44-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

commands/stat.c
@@ -12,15 +12,23 @@
 
 static int do_stat(int argc, char *argv[])
 {
-	int (*statfn)(const char *, struct stat *) = lstat;
-	int ret, opt, exitcode = 0;
+	int (*statfn)(int dirfd, const char *, struct stat *) = lstatat;
+	int ret, opt, dirfd = AT_FDCWD, extra_flags = 0, exitcode = 0;
 	char **filename;
 	struct stat st;
 
-	while((opt = getopt(argc, argv, "L")) > 0) {
+	while((opt = getopt(argc, argv, "Lc:C:")) > 0) {
 		switch(opt) {
 		case 'L':
-			statfn = stat;
+			statfn = statat;
+			break;
+		case 'C':
+			extra_flags |= O_CHROOT;
+			fallthrough;
+		case 'c':
+			dirfd = open(optarg, O_PATH | extra_flags);
+			if (dirfd < 0)
+				return dirfd;
 			break;
 		default:
 			return COMMAND_ERROR_USAGE;
@@ -31,7 +39,7 @@ static int do_stat(int argc, char *argv[])
 		return COMMAND_ERROR_USAGE;
 
 	for (filename = &argv[optind]; *filename; filename++) {
-		ret = statfn(*filename, &st);
+		ret = statfn(dirfd, *filename, &st);
 
 		if (ret) {
 			printf("%s: %s: %m\n", argv[0], *filename);
@@ -39,8 +47,10 @@ static int do_stat(int argc, char *argv[])
 			continue;
 		}
 
-		stat_print(*filename, &st);
+		stat_print(dirfd, *filename, &st);
 	}
+
+	close(dirfd);
 
 	return exitcode;
 }
@@ -51,12 +61,14 @@ BAREBOX_CMD_HELP_TEXT("or directories.")
 BAREBOX_CMD_HELP_TEXT("")
 BAREBOX_CMD_HELP_TEXT("Options:")
 BAREBOX_CMD_HELP_OPT ("-L",  "follow links")
+BAREBOX_CMD_HELP_OPT ("-c DIR",  "lookup file relative to directory DIR")
+BAREBOX_CMD_HELP_OPT ("-C DIR",  "change root to DIR before file lookup")
 BAREBOX_CMD_HELP_END
 
 BAREBOX_CMD_START(stat)
 	.cmd		= do_stat,
 	BAREBOX_CMD_DESC("display file status")
-	BAREBOX_CMD_OPTS("[-L] [FILEDIR...]")
+	BAREBOX_CMD_OPTS("[-LcC] [FILEDIR...]")
 	BAREBOX_CMD_GROUP(CMD_GRP_FILE)
 	BAREBOX_CMD_HELP(cmd_stat_help)
 BAREBOX_CMD_END

commands/Kconfig
@@ -1011,12 +1011,14 @@ config CMD_STAT
 	help
 	  Display file status
 
-	  Usage: stat [-L] [FILEDIR...]
+	  Usage: stat [-LcC] [FILEDIR...]
 
 	  Display status information about the specified files or directories.
 
 	  Options:
-		  -L	follow symlinks
+		  -L		follow symlinks
+		  -c DIR	lookup file relative to directory DIR
+		  -C DIR	change root to DIR before file lookup
 
 config CMD_MD5SUM
 	tristate

fs/fs.c
@@ -130,9 +130,8 @@ void cdev_print(const struct cdev *cdev)
 }
 EXPORT_SYMBOL(cdev_print);
 
-void stat_print(const char *filename, const struct stat *st)
+void stat_print(int dirfd, const char *filename, const struct stat *st)
 {
-	int dirfd = AT_FDCWD;
 	struct block_device *bdev = NULL;
 	struct fs_device *fdev;
 	struct cdev *cdev = NULL;
@@ -177,7 +176,7 @@ void stat_print(const char *filename, const struct stat *st)
 		char realname[PATH_MAX] = {};
 		int ret;
 
-		ret = readlink(filename, realname, PATH_MAX - 1);
+		ret = readlinkat(dirfd, filename, realname, PATH_MAX - 1);
 		if (ret)
 			printf(" -> <readlink error %pe>", ERR_PTR(ret));
 		else

include/fs.h
@@ -147,7 +147,7 @@ int ls(const char *path, ulong flags);
 
 char *mkmodestr(unsigned long mode, char *str);
 
-void stat_print(const char *filename, const struct stat *st);
+void stat_print(int dirfd, const char *filename, const struct stat *st);
 void cdev_print(const struct cdev *cdev);
 
 char *canonicalize_path(int dirfd, const char *pathname);

commit e6687733a3a056715b1547225b80361be7a6ad64

commands: stat: fix leaking file descriptors on early exit
We currently leak the dirfd if we encounter an error during option
parsing. Fix this by only recording the path at first and opening the
dirfd once all options have been parsed.

Fixes: 5b175c52b53b ("commands: stat: add option for statat")
Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>

commands/stat.c
@@ -14,6 +14,7 @@ static int do_stat(int argc, char *argv[])
 {
 	int (*statfn)(int dirfd, const char *, struct stat *) = lstatat;
 	int ret, opt, dirfd = AT_FDCWD, extra_flags = 0, exitcode = 0;
+	const char *dir = NULL;
 	char **filename;
 	struct stat st;
 
@@ -26,9 +27,7 @@ static int do_stat(int argc, char *argv[])
 			extra_flags |= O_CHROOT;
 			fallthrough;
 		case 'c':
-			dirfd = open(optarg, O_PATH | O_DIRECTORY | extra_flags);
-			if (dirfd < 0)
-				return dirfd;
+			dir = optarg;
 			break;
 		default:
 			return COMMAND_ERROR_USAGE;
@@ -37,6 +36,12 @@ static int do_stat(int argc, char *argv[])
 
 	if (optind == argc)
 		return COMMAND_ERROR_USAGE;
+
+	if (dir) {
+		dirfd = open(dir, O_PATH | O_DIRECTORY | extra_flags);
+		if (dirfd < 0)
+			return dirfd;
+	}
 
 	for (filename = &argv[optind]; *filename; filename++) {
 		ret = statfn(dirfd, *filename, &st);