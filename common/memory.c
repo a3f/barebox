d2b028fb08543063a5dcc4eac1fd0f6cca36e866
Ahmad Fatoum <a.fatoum@barebox.org>
2025-07-20 11:53:00 +0200

    Revert "ARM: MMU: map text segment ro and data segments execute never"
    This reverts commit 5916385fae838e9906b359769c51a0fa6204d583.

--- common/memory.c
+++ common/memory.c
@@ -125,14 +125,9 @@ static int mem_malloc_resource(void)
 			MEMTYPE_BOOT_SERVICES_DATA, MEMATTRS_RW);
 	request_barebox_region("barebox code",
 			(unsigned long)&_stext,
-			(unsigned long)&__start_rodata -
+			(unsigned long)&_etext -
 			(unsigned long)&_stext,
 			MEMATTRS_RX);
-	request_barebox_region("barebox RO data",
-			(unsigned long)&__start_rodata,
-			(unsigned long)&__end_rodata -
-			(unsigned long)&__start_rodata,
-			MEMATTRS_RO);
 	request_barebox_region("barebox data",
 			(unsigned long)&_sdata,
 			(unsigned long)&_edata -
