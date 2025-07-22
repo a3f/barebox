a3f/barebox

commit dd377c937f8bd14842e8e1c119bd3323f85570f8

common: fastboot: send FAIL if variable does not exist
According to doc [1], new implementations should send FAIL if the
variable specified in the getvar command does not exist.

[1]: https://android.googlesource.com/platform/system/core/+show/refs/heads/main/fastboot/README.md#121

Signed-off-by: Andrei Lalaev <andrey.lalaev@gmail.com>
Link: https://lore.barebox.org/20250611175645.144025-1-andrey.lalaev@gmail.com
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/fastboot.c
@@ -348,8 +348,10 @@ static void cb_getvar(struct fastboot *fb, const char *cmd)
 	if (fastboot_tx_print_var(fb, &fb->variables, cmd))
 		goto out;
 
-	if (!all && !partition)
-		goto skip_partitions;
+	if (!all && !partition) {
+		fastboot_tx_print(fb, FASTBOOT_MSG_FAIL, "no such variable: %s", cmd);
+		goto out;
+	}
 
 	file_list_for_each_entry(fb->files, fentry) {
 		int ret;
@@ -371,8 +373,11 @@ static void cb_getvar(struct fastboot *fb, const char *cmd)
 	if (fastboot_tx_print_var(fb, &partition_list, cmd))
 		goto out;
 
-skip_partitions:
-	fastboot_tx_print(fb, FASTBOOT_MSG_OKAY, "");
+	if (all)
+		fastboot_tx_print(fb, FASTBOOT_MSG_OKAY, "");
+	else
+		fastboot_tx_print(fb, FASTBOOT_MSG_FAIL, "no such variable: %s", cmd);
+
 out:
 	fastboot_free_variables(&partition_list);
 }

commit 2f4badf983b96988a77fecd60f86e7a61d6646ba

fastboot: populate is-logical:$partition variable
Uwe reports on IRC that fastboot tries to read some default variables
that barebox doesn't define, which triggers host-side error output:

  ERROR: fastboot: no such variable: is-logical:kernel

Flashing didn't seem impacted though.

Referring to the documentation[1], is-logical is related to a number of
commands that create/delete/resize logical partitions.

We have support for modifying partition tables in barebox, but we don't expose
it over Fastboot. Until we do, let's report all partitions as physical
partitions, which is the default value.

This issue has always existed, but only became apparent once we started
returning an error for non-existent variables.

[1]: https://android.googlesource.com/platform/system/core/+show/refs/heads/main/fastboot/README.md#237

Fixes: dd377c937f8b ("common: fastboot: send FAIL if variable does not exist")
Fixes: 3d5080aae90d ("USB: gadget: Add Android fastboot support")
Reported-by: Uwe Kleine-König <uwe@kleine-koenig.org>
Cc: Andrei Lalaev <andrey.lalaev@gmail.com>
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

common/fastboot.c
@@ -178,6 +178,8 @@ out:
 	fb_setvar(var, "%08zx", size);
 	var = fb_addvar(fb, list, "partition-type:%s", fentry->name);
 	fb_setvar(var, "%s", type);
+	var = fb_addvar(fb, list, "is-logical:%s", fentry->name);
+	fb_setvar(var, "%s", "no");
 
 	return ret;
 }