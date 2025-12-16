55baece475a8eac03b83b17d847f4f4855369171
Ahmad Fatoum <a.fatoum@pengutronix.de>
2025-12-16 10:25:42 +0100

    efi: runtime: add relocation check
    I am not happy with the way we embed the runtime services. While we make
    use of NOCROSSREFS_TO() to catch stray references, we need to list all
    sections that may not be referenced and things can slip through.
    
    Also, Clang does not support the directive. I anticipate this be solved
    properly when barebox proper is packaged as proper ELF: Then PBL would
    be responsible for applying relocations, setting up MMU protections and
    can at the same time relocate a distinct EFI runtime services DSO.
    
    Until then, let's a hacky check to verify that we don't access
    efi_runtime_data global variables through the GOT as we only have a
    single GOT and it's not marked as runtime data.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

---
+++ scripts/check-efi-runtime.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ "$#" -lt 2 ]; then
+    2>&1 echo "USAGE: $0 OUTPUT INPUT..."
+    exit 2
+fi
+
+${LD} -shared --gc-sections --whole-archive -o "$@" || {
+    2>&1 echo
+    2>&1 echo "Link check failed. Relocations to outside EFI runtime code?"
+    exit 1;
+}
