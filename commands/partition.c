a3f/barebox

commit a2b7cd183bad9c9e0888ce62f5ae0f83c070cc2a

introduce cdev
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

commands/partition.c
@@ -37,97 +37,27 @@
 #include <partition.h>
 #include <errno.h>
 #include <xfuncs.h>
+#include <fs.h>
+#include <linux/stat.h>
+#include <libgen.h>
 
-static int dev_del_partitions(struct device_d *physdev)
-{
-	struct device_d *child, *tmp;
-	struct partition *part;
-	int ret;
-
-	device_for_each_child_safe(physdev, tmp, child) {
-		if (child->type != DEVICE_TYPE_PARTITION) {
-			printf("not a partition: %s\n", child->id);
-			continue;
-		}
-
-		part = child->type_data;
-
-		if (part->flags & PARTITION_FIXED) {
-			debug("Skip fixed partition: %s\n", child->id);
-			continue;
-		}
-
-		ret = unregister_device(child);
-		if (ret) {
-			printf("delete partition `%s' failed: %s\n", child->id, errno_str());
-			return errno;
-		}
-
-		debug("deleted partition: %s\n", child->id);
-
-		free(part);
-	}
-
-	return 0;
-}
-
-static int dev_check_fixed(struct device_d *physdev, struct partition *new_part)
-{
-	struct device_d *child;
-
-	device_for_each_child(physdev, child) {
-		struct partition *part = child->type_data;
-
-		debug("check aginst partition: %s -", child->id);
-
-		if (!(part->flags & PARTITION_FIXED)) {
-			debug("  not fixed, ok\n");
-			continue;
-		}
-
-		if  (new_part->offset == part->offset &&		/* new_part is exactly part */
-		    ((new_part->device.size==0) || (new_part->device.size == part->device.size)) ) {
-			debug("  fixed, but same size, ok\n");
-			continue;
-		}
-
-		if ((new_part->offset >= part->offset &&
-		     new_part->offset < part->offset + part->device.size) ||
-		    (new_part->offset + new_part->device.size > part->offset &&
-		     new_part->offset + new_part->device.size <= part->offset + part->device.size)) {
-			printf(
-				" failed\n"
-				" partition spec %s \n"
-				"   violates fixed partition %s\n", new_part->name, child->id);
-			errno = -EINVAL;
-			return errno;
-		}
-		else
-			debug("  fixed and within limit?, ok\n");
-	}
-
-	return 0;
-}
-
-static int mtd_part_do_parse_one(struct partition *part, const char *partstr,
-				 char **endp)
+static int mtd_part_do_parse_one(char *devname, const char *partstr,
+				 char **endp, unsigned long offset,
+				 off_t devsize, size_t *retsize)
 {
 	ulong size;
 	char *end;
-	char buf[MAX_DRIVER_NAME];
+	char buf[PATH_MAX];
+	unsigned long flags = 0;
+	int ret;
 
-	memset(buf, 0, MAX_DRIVER_NAME);
+	memset(buf, 0, PATH_MAX);
 
 	if (*partstr == '-') {
-		size = part->physdev->size - part->offset;
+		size = devsize - offset;
 		end = (char *)partstr + 1;
 	} else {
 		size = strtoul_suffix(partstr, &end, 0);
-	}
-
-	if (size + part->offset > part->physdev->size) {
-		printf("partition %s end is beyond device\n", part->name);
-		return -EINVAL;
 	}
 
 	partstr = end;
@@ -139,98 +69,78 @@ static int mtd_part_do_parse_one(struct partition *part, const char *partstr,
 			printf("could not find matching ')'\n");
 			return -EINVAL;
 		}
-		if (end - partstr >= MAX_DRIVER_NAME) {
-			printf("device name too long\n");
-			return -EINVAL;
-		}
 
-		memcpy(part->name, partstr, end - partstr);
+		sprintf(buf, "%s.", devname);
+		memcpy(buf + strlen(buf), partstr, end - partstr);
+
 		end++;
+	}
+
+	if (size + offset > devsize) {
+		printf("%s: partition end is beyond device\n", buf);
+		return -EINVAL;
 	}
 
 	partstr = end;
 
 	if (*partstr == 'r' && *(partstr + 1) == 'o') {
-		part->flags |= PARTITION_READONLY;
+		flags |= PARTITION_READONLY;
 		end = (char *)(partstr + 2);
 	}
 
 	if (endp)
 		*endp = end;
 
-	strcpy(part->device.name, "partition");
-	part->device.size = size;
+	*retsize = size;
 
-	return 0;
+	ret = devfs_add_partition(devname, offset, size, flags, buf);
+	if (ret)
+		printf("cannot create %s: %s\n", buf, strerror(-ret));
+	return ret;
 }
 
 static int do_addpart(cmd_tbl_t * cmdtp, int argc, char *argv[])
 {
-	struct partition *part;
-	struct device_d *dev;
+	char *devname;
 	char *endp;
-	int num = 0;
-	unsigned long offset;
+	unsigned long offset = 0;
+	off_t devsize;
+	struct stat s;
 
 	if (argc != 3) {
 		printf("Usage:\n  %s\n", cmdtp->usage);
 		return 1;
 	}
 
-	dev = get_device_by_path(argv[1]);
-	if (!dev) {
-		printf("no such device: %s\n", argv[1]);
+	if (stat(argv[1], &s)) {
+		perror("addpart");
 		return 1;
 	}
+	devsize = s.st_size;
 
-	dev_del_partitions(dev);
-
-	offset = 0;
+	devname = basename(argv[1]);
 
 	endp = argv[2];
 
 	while (1) {
-		part = xzalloc(sizeof(struct partition));
+		size_t size = 0;
 
-		part->offset = offset;
-		part->physdev = dev;
-		part->num = num;
-		part->device.map_base = dev->map_base + offset;
-		part->device.type_data = part;
-		part->device.type = DEVICE_TYPE_PARTITION; 
+		if (mtd_part_do_parse_one(devname, endp, &endp, offset, devsize, &size))
+			return 1;
 
-		if (mtd_part_do_parse_one(part, endp, &endp))
-			goto free_out;
-
-		if (dev_check_fixed(dev, part))
-			goto free_out;
-
-		sprintf(part->device.id, "%s.%s", dev->id, part->name);
-		if (register_device(&part->device))
-			goto free_out;
-
-		dev_add_child(dev, &part->device);
-
-		offset += part->device.size;
-		num++;
+		offset += size;
 
 		if (!*endp)
 			break;
 
 		if (*endp != ',') {
 			printf("parse error\n");
-			goto err_out;
+			return 1;
 		}
 		endp++;
 	}
 
 	return 0;
-
-free_out:
-	free(part);
-err_out:
-	dev_del_partitions(dev);
-	return 1;
 }
 
 static const __maybe_unused char cmd_addpart_help[] =
@@ -275,38 +185,33 @@ U_BOOT_CMD_END
 
 static int do_delpart(cmd_tbl_t * cmdtp, int argc, char *argv[])
 {
-	struct device_d *dev;
+	int i, err;
 
-	if (argc != 2) {
-		printf("Usage:\n%s\n", cmdtp->usage);
-		return 1;
+	for (i = 1; i < argc; i++) {
+		err = devfs_del_partition(basename(argv[i]));
+		if (err) {
+			printf("cannot delete %s: %s\n", argv[i], strerror(-err));
+			break;
+		}
 	}
 
-	dev = get_device_by_path(argv[1]);
-	if (!dev) {
-		printf("no such device: %s\n", argv[1]);
-		return 1;
-	}
-
-	dev_del_partitions(dev);
-
-	return 0;
+	return 1;
 }
 
 static const __maybe_unused char cmd_delpart_help[] =
-"Usage: delpart <device>\n"
+"Usage: delpart FILE...\n"
 "Delete partitions previously added to a device with addpart.\n";
 
 U_BOOT_CMD_START(delpart)
-	.maxargs = 2,
+	.maxargs = CONFIG_MAXARGS,
 	.cmd = do_delpart,
-	.usage = "delete a partition table from a device",
+	.usage = "delete partition(s)",
 	U_BOOT_CMD_HELP(cmd_delpart_help)
 U_BOOT_CMD_END
 
 /** @page delpart_command delpart Delete a partition
  *
- * Usage is: delpart \<device>
+ * Usage is: delpart \<partions>
  *
  * Delete a partition previously added to a device with addpart.
  */

common/partition.c
@@ -1,358 +1,0 @@
-/*
- * (C) 2007 Pengutronix, Sascha Hauer <s.hauer@pengutronix.de>
- *
- * See file CREDITS for list of people who contributed to this
- * project.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- * MA 02111-1307 USA
- */
-
-/**
- * @file
- * @brief Partition handling on top of devices
- */
-
-#include <common.h>
-#include <init.h>
-#include <driver.h>
-#include <malloc.h>
-#include <errno.h>
-#include <partition.h>
-#include <xfuncs.h>
-#include <ioctl.h>
-#include <nand.h>
-#include <linux/mtd/mtd-abi.h>
-
-/**
- * Add one partition on top of a device, as a device.
- * @param[in] dev The device to add a partition on
- * @param[in] offset Start offset of the partition inside the whole device
- * @param[in] size Size of the new partition
- * @param[in] flags FIXME
- * @param[in] name Partition name (can be obtained with devinfo command)
- * @return The device representing the new partition.
- */
-struct device_d *dev_add_partition(struct device_d *dev, unsigned long offset,
-		size_t size, int flags, const char *name)
-{
-	struct partition *part;
-
-	if (offset + size > dev->size)
-		return NULL;
-
-	part = xzalloc(sizeof(struct partition));
-
-	strcpy(part->device.name, "partition");
-	part->device.map_base = dev->map_base + offset;
-	part->device.size = size;
-	part->device.type_data = part;
-	part->device.type = DEVICE_TYPE_PARTITION;
-	get_free_deviceid(part->device.id, name);
-
-	part->offset = offset;
-	part->physdev = dev;
-	part->flags = flags;
-
-	register_device(&part->device);
-	dev_add_child(dev, &part->device);
-
-	if (part->device.driver)
-		return &part->device;
-
-	unregister_device(&part->device);
-	free(part);
-	return 0;
-}
-
-/**
- * Erase partition content.
- * @param[in] dev The partition info as a device
- * @param[in] count Length in bytes to erase
- * @param[in] offset Start offset within the partition
- * @return -1 if no erase feature for this device is available, anything else
- * the erase function returns.
- */
-static int part_erase(struct device_d *dev, size_t count, unsigned long offset)
-{
-	struct partition *part = dev->type_data;
-
-	if (part->flags & PARTITION_READONLY)
- 		return -EROFS;
-
-	return dev_erase(part->physdev, count, offset + part->offset);
-}
-
-/**
- * Protecting a partition.
- * @param[in] dev The partition info as a device
- * @param[in] count Length in bytes to protect
- * @param[in] offset Start offset within the partition
- * @param[in] prot FIXME
- * @return -1 if FIXME.
- */
-static int part_protect(struct device_d *dev, size_t count, unsigned long offset, int prot)
-{
-	struct partition *part = dev->type_data;
-
-	return dev_protect(part->physdev, count, offset + part->offset, prot);
-}
-
-/**
- * FIXME.
- * @param[in] dev The partition info as a device
- * @param[in] map FXIME
- * @param[in] flags FIXME
- * @return -1 if FIXME.
- */
-static int part_memmap(struct device_d *dev, void **map, int flags)
-{
-	struct partition *part = dev->type_data;
-	int ret;
-
-	if (part->physdev->driver->memmap) {
-		ret = part->physdev->driver->memmap(part->physdev, map, flags);
-		if (ret)
-			return ret;
-		*map = (void *)((unsigned long)*map + part->offset);
-		return 0;
-	}
-
-	return -ENOSYS;
-}
-
-/**
- * FIXME.
- * @param[in] dev The partition info as a device
- * @param[in] buf FXIME
- * @param[in] count FXIME
- * @param[in] offset FXIME
- * @param[in] flags FIXME
- * @return FIXME.
- */
-static ssize_t part_read(struct device_d *dev, void *buf, size_t count,
-	unsigned long offset, ulong flags)
-{
-	struct partition *part = dev->type_data;
-
-	return dev_read(part->physdev, buf, count, offset + part->offset, flags);
-}
-
-/**
- * FIXME.
- * @param[in] dev The partition info as a device
- * @param[in] buf FXIME
- * @param[in] count FXIME
- * @param[in] offset FXIME
- * @param[in] flags FIXME
- * @return FIXME.
- */
-static ssize_t part_write(struct device_d *dev, const void *buf, size_t count,
-	unsigned long offset, ulong flags)
-{
-	struct partition *part = dev->type_data;
-
-	if (part->flags & PARTITION_READONLY)
- 		return -EROFS;
-	else
-		return dev_write(part->physdev, buf, count, offset + part->offset, flags);
-}
-
-static off_t part_lseek(struct device_d *dev, off_t ofs)
-{
-	struct partition *part = dev->type_data;
-
-	return dev_lseek(part->physdev, ofs);
-}
-
-static int part_open(struct device_d *dev, struct filep *f)
-{
-	struct partition *part = dev->type_data;
-
-	return dev_open(part->physdev, f);
-}
-
-static int part_close(struct device_d *dev, struct filep *f)
-{
-	struct partition *part = dev->type_data;
-
-	return dev_close(part->physdev, f);
-}
-
-static int part_ioctl(struct device_d *dev, int request,
-	void *buf)
-{
-	struct partition *part = dev->type_data;
-	off_t offset;
-
-	switch (request) {
-	case MEMSETBADBLOCK:
-	case MEMGETBADBLOCK:
-		offset = (off_t)buf;
-		offset += part->offset;
-		return dev_ioctl(part->physdev, request, (void *)offset);
-	case MEMGETINFO:
-		return dev_ioctl(part->physdev, request, buf);
-	}
-
-	return -ENOSYS;
-}
-
-/**
- * FIXME.
- * @param[in] dev The partition info as a device
- * @return FIXME.
- */
-static int part_probe(struct device_d *dev)
-{
-#ifdef DEBUG
-	struct partition *part = dev->type_data;
-#endif
-
-	debug("registering partition %s on device %s (size=0x%08x, name=%s)\n",
-			dev->id, part->physdev->id, dev->size, part->name);
-	return 0;
-}
-
-/**
- * FIXME.
- * @param[in] dev The partition info as a device
- */
-static void part_remove(struct device_d *dev)
-{
-}
-
-/**
- * Partition driver description
- */
-struct driver_d part_driver = {
-	.name  	= "partition",
-	.probe 	= part_probe,
-	.remove = part_remove,
-	.open   = part_open,
-	.close  = part_close,
-	.ioctl  = part_ioctl,
-	.read  	= part_read,
-	.write 	= part_write,
-	.lseek  = part_lseek,
-	.erase 	= part_erase,
-	.protect= part_protect,
-	.memmap = part_memmap,
-	.type	= DEVICE_TYPE_PARTITION,
-};
-
-static int partition_init(void)
-{
-	return register_driver(&part_driver);
-}
-
-device_initcall(partition_init);
-
-/**
-@page partitions Partition Handling
-
-Partitions are runtime informartion only, not permanent. So they must be set
-everytime the system starts. The required command can be embedded into the
-default environment.
-
-@note Partitions defined in this way are intended to be used with the kernel
-command line partition parsing feature. In Uboot2 these types of partitions are
-handled in the same way as every other device.
-
-@par The addpart command
-
-What we want:
-
-@verbatim
- device nor0
-    |--- partition 0
-    |--- partition 1
-    |--- partition 2
-    |--- partition 3
-    `--- partition 4
-@endverbatim
-
-How to get:
-
-@verbatim
-$ addpart /dev/nor0 256k(uboot),128k(env),256k(bla),1024k(blubb),2048k(friesel)
-$ devinfo
- |---nor0.uboot
- |---nor0.env
- |---nor0.bla
- |---nor0.blubb
- |---nor0.friesel
-@endverbatim
-
-@par Partitions with sub partitions:
-
-Partitions are based on devices. And partitions will result into devices. So
-there is a way to create sub partitions on partitions.
-
-What we want:
-
-@verbatim
- device nor0
-    |--- partition 0
-    |--- partition 1
-    |--- partition 2
-    |--- partition 3
-    `--- partition 4
-           |--- partition 0
-           `--- partition 1
-@endverbatim
-
-How to get:
-
-@verbatim
-$ addpart /dev/nor0 256k(uboot),128k(env),256k(bla),1M(blubb),2048k(friesel)
-$ devinfo
- |---nor0.uboot
- |---nor0.env
- |---nor0.bla
- |---nor0.blubb
- |---nor0.friesel
-
-$ addpart /dev/nor0.friesel 1024(fussel),1024k(boerks)
-$ devinfo
- |---nor0.uboot
- |---nor0.env
- |---nor0.bla
- |---nor0.blubb
- |---nor0.friesel
-        |---nor0.friesel.fussel
-        `---nor0.friesel.boerks
-@endverbatim
-
-@par Forwarding partitions to the kernel:
-
-@verbatim
-$ device="nor0"
-$ partitions="256k(uboot),128k(env),256k(bla),1024k(blubb),2048k(friesel)"
-$ addpart /dev/$device:$partitions
-@endverbatim
-
-@par Removing partitions:
-
-As partitions are a logically information only, they can be removed from a
-device at runtime. You can't remove a single partition within others on the
-same device. Only all partitions on the given device can be removed with this
-command.
-
-As sub partitions occure as devices you also can remove sub partitions from
-their parent in this way. Partitions cannot be removed as long as they are
-mounted or have subpartitions.
-
-*/

board/pcm037/pcm037.c
@@ -43,7 +43,6 @@
  */
 static struct device_d cfi_dev = {
 	.name     = "cfi_flash",
-	.id       = "nor0",
 	.map_base = IMX_CS0_BASE,
 	.size     = 32 * 1024 * 1024,	/* area size */
 };
@@ -245,10 +244,9 @@ static int imx31_devices_init(void)
 	 * Create partitions that should be
 	 * not touched by any regular user
 	 */
-#ifdef CONFIG_PARTITION
-	dev_add_partition(&cfi_dev, 0x00000, 0x40000, PARTITION_FIXED, "self");	/* ourself */
-	dev_add_partition(&cfi_dev, 0x40000, 0x20000, PARTITION_FIXED, "env");	/* environment */
-#endif
+	devfs_add_partition("nor0", 0x00000, 0x40000, PARTITION_FIXED, "self0");	/* ourself */
+	devfs_add_partition("nor0", 0x40000, 0x20000, PARTITION_FIXED, "env0");	/* environment */
+
 	dev_protect(&cfi_dev, 0x20000, 0, 1);
 
 	register_device(&sram_dev);

board/pcm038/pcm038.c
@@ -111,9 +111,9 @@ static struct device_d nand_dev = {
 
 static int pcm038_devices_init(void)
 {
-	struct device_d *nand, *dev;
-	char *envdev = "no";
 	int i;
+	struct device_d *nand;
+	char *envdev = "no";
 
 	unsigned int mode[] = {
 		PD0_AIN_FEC_TXD0,
@@ -192,20 +192,16 @@ static int pcm038_devices_init(void)
 		nand = get_device_by_path("/dev/nand0");
 		if (!nand)
 			break;
-		dev = dev_add_partition(nand, 0x00000, 0x40000, PARTITION_FIXED, "self_raw");
-		if (!dev)
-			break;
-		dev_add_bb_dev(dev, "self0");
+		devfs_add_partition("nand0", 0x00000, 0x40000, PARTITION_FIXED, "self_raw");
+		dev_add_bb_dev("self_raw", "self0");
 
-		dev = dev_add_partition(nand, 0x40000, 0x20000, PARTITION_FIXED, "env_raw");
-		if (!dev)
-			break;
-		dev_add_bb_dev(dev, "env0");
+		devfs_add_partition("nand0", 0x40000, 0x20000, PARTITION_FIXED, "env_raw");
+		dev_add_bb_dev("env_raw", "env0");
 		envdev = "NAND";
 		break;
 	default:
-		dev_add_partition(&cfi_dev, 0x00000, 0x40000, PARTITION_FIXED, "self");
-		dev_add_partition(&cfi_dev, 0x40000, 0x20000, PARTITION_FIXED, "env");
+		devfs_add_partition("nor0", 0x00000, 0x40000, PARTITION_FIXED, "self0");
+		devfs_add_partition("nor0", 0x40000, 0x20000, PARTITION_FIXED, "env0");
 		dev_protect(&cfi_dev, 0x40000, 0, 1);
 		envdev = "NOR";
 	}

board/scb9328/scb9328.c
@@ -35,20 +35,24 @@
 
 static struct device_d cfi_dev = {
 	.name     = "cfi_flash",
-	.id       = "nor0",
 
 	.map_base = 0x10000000,
 	.size     = 16 * 1024 * 1024,
 };
 
+static struct memory_platform_data sdram_pdata = {
+	.name = "ram0",
+	.flags = DEVFS_RDWR,
+};
+
 static struct device_d sdram_dev = {
-	.name     = "ram",
-	.id       = "ram0",
+	.name     = "mem",
 
 	.map_base = 0x08000000,
 	.size     = 16 * 1024 * 1024,
 
 	.type     = DEVICE_TYPE_DRAM,
+	.platform_data = &sdram_pdata,
 };
 
 static struct dm9000_platform_data dm9000_data = {
@@ -93,8 +97,8 @@ static int scb9328_devices_init(void) {
 	register_device(&sdram_dev);
 	register_device(&dm9000_dev);
 
-	dev_add_partition(&cfi_dev, 0x00000, 0x20000, PARTITION_FIXED, "self");
-	dev_add_partition(&cfi_dev, 0x40000, 0x20000, PARTITION_FIXED, "env");
+	devfs_add_partition("nor0", 0x00000, 0x20000, PARTITION_FIXED, "self0");
+	devfs_add_partition("nor0", 0x40000, 0x20000, PARTITION_FIXED, "env0");
 	dev_protect(&cfi_dev, 0x20000, 0, 1);
 
 	armlinux_set_bootparams((void *)0x08000100);

commands/mem.c
@@ -518,44 +518,46 @@ U_BOOT_CMD_START(memcpy)
 	U_BOOT_CMD_HELP(cmd_memcpy_help)
 U_BOOT_CMD_END
 
-static struct device_d mem_dev = {
-	.name  = "mem",
-	.id    = "mem",
-	.map_base = 0,
-	.size   = ~0, /* FIXME: should be 0x100000000, ahem... */
+static struct file_operations memops = {
+	.read  = mem_read,
+	.write = mem_write,
+	.memmap = generic_memmap_rw,
+	.lseek = dev_lseek_default,
 };
+
+static int mem_probe(struct device_d *dev)
+{
+	struct memory_platform_data *pdata = dev->platform_data;
+	struct cdev *cdev;
+
+	cdev = xzalloc(sizeof (*cdev));
+	dev->priv = cdev;
+
+	cdev->name = pdata->name;
+	cdev->size = dev->size;
+	cdev->ops = &memops;
+	cdev->dev = dev;
+
+	devfs_create(cdev);
+
+	return 0;
+}
 
 static struct driver_d mem_drv = {
 	.name  = "mem",
-	.probe = dummy_probe,
-	.open  = dev_open_default,
-	.close = dev_close_default,
-	.read  = mem_read,
-	.write = mem_write,
-	.memmap = mem_memmap,
-	.lseek = dev_lseek_default,
+	.probe = mem_probe,
 };
 
-static struct driver_d ram_drv = {
-	.name  = "ram",
-	.probe = dummy_probe,
-	.open  = dev_open_default,
-	.close = dev_close_default,
-	.read  = mem_read,
-	.write = mem_write,
-	.lseek = dev_lseek_default,
-	.memmap = mem_memmap,
-	.type  = DEVICE_TYPE_DRAM,
+static struct memory_platform_data mem_dev_pdata = {
+	.name = "mem",
+	.flags = DEVFS_RDWR,
 };
 
-static struct driver_d rom_drv = {
-	.name  = "rom",
-	.probe = dummy_probe,
-	.open  = dev_open_default,
-	.close = dev_close_default,
-	.read  = mem_read,
-	.memmap = mem_memmap,
-	.lseek = dev_lseek_default,
+static struct device_d mem_dev = {
+	.name  = "mem",
+	.map_base = 0,
+	.size   = ~0, /* FIXME: should be 0x100000000, ahem... */
+	.platform_data = &mem_dev_pdata,
 };
 
 static int mem_init(void)
@@ -566,10 +568,9 @@ static int mem_init(void)
 		return -1;
 	}
 
-	register_device(&mem_dev);
 	register_driver(&mem_drv);
-	register_driver(&ram_drv);
-	register_driver(&rom_drv);
+	register_device(&mem_dev);
+
 	return 0;
 }
 

commands/nand.c
@@ -17,7 +17,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 #include <common.h>
 #include <command.h>
 #include <fs.h>
@@ -31,40 +30,45 @@
 #include <nand.h>
 #include <linux/mtd/mtd-abi.h>
 #include <fcntl.h>
+#include <libgen.h>
 
 struct nand_bb {
-	struct device_d device;
-
-	struct device_d *physdev;
-
+	char *devname;
+	char *name;
 	int open;
 
 	struct mtd_info_user info;
 
+	size_t raw_size;
+	size_t size;
+	int fd;
 	off_t offset;
+
+	struct cdev cdev;
 };
 
-static ssize_t nand_bb_read(struct device_d *dev, void *buf, size_t count,
+static ssize_t nand_bb_read(struct cdev *cdev, void *buf, size_t count,
 	unsigned long offset, ulong flags)
 {
-	struct nand_bb *bb = dev->priv;
+	struct nand_bb *bb = cdev->priv;
 	int ret, bytes = 0, now;
 
 	debug("%s %d %d\n", __func__, offset, count);
 
 	while(count) {
-		ret = dev_ioctl(bb->physdev, MEMGETBADBLOCK, (void *)bb->offset);
+		ret = ioctl(bb->fd, MEMGETBADBLOCK, (void *)bb->offset);
 		if (ret < 0)
 			return ret;
 
 		if (ret) {
-			debug("skipping bad block at 0x%08x\n", bb->offset);
+			printf("skipping bad block at 0x%08x\n", bb->offset);
 			bb->offset += bb->info.erasesize;
 		}
 
 		now = min(count, (size_t)(bb->info.erasesize -
 				(bb->offset % bb->info.erasesize)));
-		ret = dev_read(bb->physdev, buf, now, bb->offset, flags);
+		lseek(bb->fd, bb->offset, SEEK_SET);
+		ret = read(bb->fd, buf, now);
 		if (ret < 0)
 			return ret;
 		buf += now;
@@ -76,16 +80,16 @@ static ssize_t nand_bb_read(struct device_d *dev, void *buf, size_t count,
 	return bytes;
 }
 
-static ssize_t nand_bb_write(struct device_d *dev, const void *buf, size_t count,
+static ssize_t nand_bb_write(struct cdev *cdev, const void *buf, size_t count,
 	unsigned long offset, ulong flags)
 {
-	struct nand_bb *bb = dev->priv;
+	struct nand_bb *bb = cdev->priv;
 	int ret, bytes = 0, now;
 
-	debug("%s %d %d\n", __func__, offset, count);
+	debug("%s offset: 0x%08x count: 0x%08x\n", __func__, offset, count);
 
 	while(count) {
-		ret = dev_ioctl(bb->physdev, MEMGETBADBLOCK, (void *)bb->offset);
+		ret = ioctl(bb->fd, MEMGETBADBLOCK, (void *)bb->offset);
 		if (ret < 0)
 			return ret;
 
@@ -96,7 +100,8 @@ static ssize_t nand_bb_write(struct device_d *dev, const void *buf, size_t count
 
 		now = min(count, (size_t)(bb->info.erasesize -
 				(bb->offset % bb->info.erasesize)));
-		ret = dev_write(bb->physdev, buf, now, bb->offset, flags);
+		lseek(bb->fd, offset, SEEK_SET);
+		ret = write(bb->fd, buf, now);
 		if (ret < 0)
 			return ret;
 		buf += now;
@@ -108,21 +113,21 @@ static ssize_t nand_bb_write(struct device_d *dev, const void *buf, size_t count
 	return bytes;
 }
 
-static int nand_bb_erase(struct device_d *dev, size_t count, unsigned long offset)
+static int nand_bb_erase(struct cdev *cdev, size_t count, unsigned long offset)
 {
-	struct nand_bb *bb = dev->priv;
+	struct nand_bb *bb = cdev->priv;
 
-	if (offset != 0 || count != dev->size) {
+	if (offset != 0 || count != bb->raw_size) {
 		printf("can only erase whole device\n");
 		return -EINVAL;
 	}
 
-	return dev_erase(bb->physdev, dev->size, 0);
+	return erase(bb->fd, bb->raw_size, 0);
 }
 
-static int nand_bb_open(struct device_d *dev, struct filep *f)
+static int nand_bb_open(struct cdev *cdev, struct filep *f)
 {
-	struct nand_bb *bb = dev->priv;
+	struct nand_bb *bb = cdev->priv;
 
 	if (bb->open)
 		return -EBUSY;
@@ -133,9 +138,9 @@ static int nand_bb_open(struct device_d *dev, struct filep *f)
 	return 0;
 }
 
-static int nand_bb_close(struct device_d *dev, struct filep *f)
+static int nand_bb_close(struct cdev *cdev, struct filep *f)
 {
-	struct nand_bb *bb = dev->priv;
+	struct nand_bb *bb = cdev->priv;
 
 	bb->open = 0;
 
@@ -147,12 +152,12 @@ static int nand_bb_calc_size(struct nand_bb *bb)
 	ulong pos = 0;
 	int ret;
 
-	while (pos < bb->physdev->size) {
-		ret = dev_ioctl(bb->physdev, MEMGETBADBLOCK, (void *)pos);
+	while (pos < bb->raw_size) {
+		ret = ioctl(bb->fd, MEMGETBADBLOCK, (void *)pos);
 		if (ret < 0)
 			return ret;
 		if (!ret)
-			bb->device.size += bb->info.erasesize;
+			bb->cdev.size += bb->info.erasesize;
 
 		pos += bb->info.erasesize;
 	}
@@ -160,39 +165,13 @@ static int nand_bb_calc_size(struct nand_bb *bb)
 	return 0;
 }
 
-static int nand_bb_probe(struct device_d *dev)
-{
-	struct nand_bb *bb = dev->priv;
-	int ret;
-
-	ret = dev_ioctl(bb->physdev, MEMGETINFO, &bb->info);
-	if (ret)
-		return ret;
-	return nand_bb_calc_size(bb);
-}
-
-static void nand_bb_remove(struct device_d *dev)
-{
-}
-
-struct driver_d nand_bb_driver = {
-	.name  	= "nand_bb",
-	.probe 	= nand_bb_probe,
-	.remove = nand_bb_remove,
+static struct file_operations nand_bb_ops = {
 	.open   = nand_bb_open,
 	.close  = nand_bb_close,
 	.read  	= nand_bb_read,
 	.write 	= nand_bb_write,
 	.erase	= nand_bb_erase,
-	.type	= DEVICE_TYPE_NAND_BB,
 };
-
-static int nand_bb_init(void)
-{
-	return register_driver(&nand_bb_driver);
-}
-
-device_initcall(nand_bb_init);
 
 /**
  * Add a bad block aware device ontop of another (NAND) device 
@@ -200,39 +179,53 @@ device_initcall(nand_bb_init);
  * @param[in] name Partition name (can be obtained with devinfo command)
  * @return The device representing the new partition.
  */
-struct device_d *dev_add_bb_dev(struct device_d *dev, const char *name)
+int dev_add_bb_dev(char *path, const char *name)
 {
 	struct nand_bb *bb;
+	int ret;
+	struct stat s;
 
 	bb = xzalloc(sizeof(*bb));
-
+	bb->devname = asprintf("/dev/%s", basename(path));
 	if (name)
-		strcpy(bb->device.id, name);
+		bb->cdev.name = strdup(name);
 	else
-		sprintf(bb->device.id, "%s.bb", dev->id);
-	strcpy(bb->device.name, "nand_bb");
-	bb->device.priv = bb;
-	bb->device.size = 0;
-	bb->device.type	= DEVICE_TYPE_NAND_BB;
-	bb->physdev = dev;
+		bb->cdev.name = asprintf("%s.bb", basename(path));
 
-	if (register_device(&bb->device))
+	ret = stat(bb->devname, &s);
+	if (ret)
 		goto free_out;
 
-	dev_add_child(dev, &bb->device);
+	bb->raw_size = s.st_size;
 
-	return &bb->device;
+	bb->fd = open(bb->devname, O_RDWR);
+	if (bb->fd < 0) {
+		ret = -ENODEV;
+		goto free_out;
+	}
+
+	ret = ioctl(bb->fd, MEMGETINFO, &bb->info);
+	if (ret)
+		goto free_out;
+
+	nand_bb_calc_size(bb);
+	bb->cdev.ops = &nand_bb_ops;
+	bb->cdev.priv = bb;
+
+	devfs_create(&bb->cdev);
+
+	return 0;
 
 free_out:
 	free(bb);
-	return 0;
+	return ret;
 }
 
 #define NAND_ADD (1 << 0)
 #define NAND_DEL (1 << 1)
 #define NAND_MARKBAD (1 << 2)
 
-static int do_nand (cmd_tbl_t *cmdtp, int argc, char *argv[])
+static int do_nand(cmd_tbl_t *cmdtp, int argc, char *argv[])
 {
 	int opt;
 	struct device_d *dev;
@@ -262,13 +255,7 @@ static int do_nand (cmd_tbl_t *cmdtp, int argc, char *argv[])
 
 	if (command & NAND_ADD) {
 		while (optind < argc) {
-			dev = get_device_by_path(argv[optind]);
-			if (!dev) {
-				printf("no such device: %s\n", argv[optind]);
-				return 1;
-			}
-
-			if (!dev_add_bb_dev(dev, NULL))
+			if (dev_add_bb_dev(argv[optind], NULL))
 				return 1;
 
 			optind++;
@@ -277,17 +264,20 @@ static int do_nand (cmd_tbl_t *cmdtp, int argc, char *argv[])
 
 	if (command & NAND_DEL) {
 		while (optind < argc) {
-			dev = get_device_by_path(argv[optind]);
-			if (!dev) {
+			struct cdev *cdev;
+			cdev = cdev_by_name(basename(argv[optind]));
+			if (!cdev) {
 				printf("no such device: %s\n", argv[optind]);
 				return 1;
 			}
+			dev = cdev->dev;
 
 			if (dev->type != DEVICE_TYPE_NAND_BB) {
 				printf("not a nand bb device: %s\n", dev);
 				return 1;
 			}
 			bb = dev->priv;
+			close(bb->fd);
 			unregister_device(dev);
 			free(bb);
 			optind++;

common/Makefile
@@ -12,7 +12,6 @@ obj-y += clock.o
 obj-y += command.o
 obj-$(CONFIG_CONSOLE_FULL) += console.o
 obj-$(CONFIG_CONSOLE_SIMPLE) += console_simple.o
-obj-$(CONFIG_PARTITION) += partition.o
 obj-y += env.o
 obj-y += startup.o
 obj-y += misc.o

commit aa367be9bb808d3745ec794a8f8e8c2aa315dbf0

commands: delpart: return success when no error occurred
Since the introduction of cdevs, this command returns 1 unconditionally.
Fix that up to return 0 on success and a more descriptive code
otherwise.

Fixes: a2b7cd183bad ("introduce cdev")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

commands/partition.c
@@ -91,11 +91,11 @@ static int do_delpart(int argc, char *argv[])
 		err = devfs_del_partition(kbasename(argv[i]));
 		if (err) {
 			printf("cannot delete %s: %pe\n", argv[i], ERR_PTR(err));
-			break;
+			return err;
 		}
 	}
 
-	return 1;
+	return 0;
 }
 
 BAREBOX_CMD_HELP_START(delpart)