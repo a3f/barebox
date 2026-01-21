69c7869315e62b6a3dcf7085cfc5bcb8a73ac47a
Ahmad Fatoum <a.fatoum@pengutronix.de>
2026-01-21 15:10:52 +0100

    bootm: give bootm_load_ functions an end address
    So far we expected that a load operation will not overflow, but there
    was actually no way for the callee to check this.
    
    As the architecture-specific bootm handlers are tasked with allocating
    memory for boot artifacts have them pass along not only the start of the
    load region, but also its end, so the callee can check that it didn't
    overflow.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

--- arch/arm/lib64/armlinux.c
+++ arch/arm/lib64/armlinux.c
@@ -43,6 +43,7 @@ static int do_bootm_barebox(struct image_data *data)
 {
 	void (*fn)(unsigned long x0, unsigned long x1, unsigned long x2,
 		       unsigned long x3);
+	const struct resource *os_res;
 	resource_size_t start, end;
 	unsigned long barebox;
 	int ret;
@@ -53,9 +54,9 @@ static int do_bootm_barebox(struct image_data *data)
 
 	barebox = PAGE_ALIGN(start);
 
-	ret = bootm_load_os(data, barebox);
-	if (ret)
-		goto out;
+	os_res = bootm_load_os(data, barebox, end);
+	if (IS_ERR(os_res))
+		return PTR_ERR(os_res);
 
 	printf("Loaded barebox image to 0x%08lx\n", barebox);
 
