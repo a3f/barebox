b1b13f4747d8b8863fd412f4cad2aafa991c70df
Johannes Schauer Marin Rodrigues <josch@mister-muffin.de>
2026-06-18 22:43:22 +0200

    build.sh: set SOURCE_DATE_EPOCH to last git commit timestamp
    Derive KBUILD_BUILD_TIMESTAMP from it and also set KBUILD_BUILD_USER and
    KBUILD_BUILD_HOST.
    
    Thanks: Ahmad Fatoum <a.fatoum@pengutronix.de>

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 
 set -exu
 
+if [ -z ${SOURCE_DATE_EPOCH:+x} ] && git -C . rev-parse 2>/dev/null; then
+	SOURCE_DATE_EPOCH=$(git log -1 --format=%ct)
+	export SOURCE_DATE_EPOCH
+fi
+export KBUILD_BUILD_TIMESTAMP="@$(SOURCE_DATE_EPOCH)"
+export KBUILD_BUILD_USER=builduser
+export KBUILD_BUILD_HOST=buildhost
+
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
 cp mnt-reform-defconfig .config
