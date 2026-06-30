854ddfec2033a370694b9a0dc38647210675f8c5
Ahmad Fatoum <a.fatoum@barebox.org>
2026-06-30 09:48:08 +0200

    input: matrix-keymap: sync with Linux
    The current kernel API has matrix_keypad_parse_properties(), which
    parses the "keypad,num-rows" and "keypad,num-columns" properties and
    extends matrix_keypad_build_keymap() to take the results.
    
    Import the kernel-side changes to make future driver porting easier.
    The i.MX keypad driver is the only user and is switched over to make use
    of the new API.
    
    No functional change intended.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>

--- drivers/input/matrix-keymap.c
+++ drivers/input/matrix-keymap.c
@@ -1,81 +1,204 @@
 // SPDX-License-Identifier: GPL-2.0-only
+// SPDX-Comment: Origin-URL: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/drivers/input/matrix-keymap.c?id=40a6bb10d3d124c9adfd9f5e0bb8e020e0ccb712
+// SPDX-FileCopyrightText: 2012 Google, Inc
+/*
+ * Helpers for matrix keyboard bindings
+ *
+ * Author:
+ *	Olof Johansson <olof@lixom.net>
+ */
 
-#include <common.h>
+#include <linux/device.h>
+#include <linux/export.h>
+#include <linux/gfp.h>
+#include <linux/input.h>
 #include <linux/input/matrix_keypad.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <of.h>
+#include <linux/types.h>
 
-static int matrix_keypad_parse_of_keymap(struct device *dev,
-					 unsigned int row_shift,
-					 unsigned short *keymap)
+static bool matrix_keypad_map_key(struct input_dev *input_dev,
+				  unsigned int rows, unsigned int cols,
+				  unsigned int row_shift, unsigned int key,
+				  unsigned short *keymap)
 {
-	unsigned int proplen, i, size;
-	const __be32 *prop;
-	struct device_node *np = dev->of_node;
-	const char *propname = "linux,keymap";
+	unsigned int row = KEY_ROW(key);
+	unsigned int col = KEY_COL(key);
+	unsigned short code = KEY_VAL(key);
 
-	prop = of_get_property(np, propname, &proplen);
-	if (!prop) {
-		dev_err(dev, "OF: %s property not defined in %pOF\n",
-			propname, np);
-		return -ENOENT;
+	if (row >= rows || col >= cols) {
+		dev_err(input_dev->parent,
+			"%s: invalid keymap entry 0x%x (row: %d, col: %d, rows: %d, cols: %d)\n",
+			__func__, key, row, col, rows, cols);
+		return false;
 	}
 
-	if (proplen % sizeof(u32)) {
-		dev_err(dev, "OF: Malformed keycode property %s in %pOF\n",
-			propname, np);
+	keymap[MATRIX_SCAN_CODE(row, col, row_shift)] = code;
+
+	return true;
+}
+
+/**
+ * matrix_keypad_parse_properties() - Read properties of matrix keypad
+ *
+ * @dev: Device containing properties
+ * @rows: Returns number of matrix rows
+ * @cols: Returns number of matrix columns
+ * @return 0 if OK, <0 on error
+ */
+int matrix_keypad_parse_properties(struct device *dev,
+				   unsigned int *rows, unsigned int *cols)
+{
+	struct device_node *np = dev_of_node(dev);
+	*rows = *cols = 0;
+
+	if (!np)
+		return -ENODEV;
+
+	of_property_read_u32(np, "keypad,num-rows", rows);
+	of_property_read_u32(np, "keypad,num-columns", cols);
+
+	if (!*rows || !*cols) {
+		dev_err(dev, "number of keypad rows/columns not specified\n");
 		return -EINVAL;
-	}
-
-	size = proplen / sizeof(u32);
-
-	for (i = 0; i < size; i++) {
-		unsigned int key = be32_to_cpup(prop + i);
-
-		unsigned int row = KEY_ROW(key);
-		unsigned int col = KEY_COL(key);
-		unsigned short code = KEY_VAL(key);
-
-		if (row >= MATRIX_MAX_ROWS || col >= MATRIX_MAX_COLS) {
-			dev_err(dev, "rows/cols out of range\n");
-			continue;
-		}
-
-		keymap[MATRIX_SCAN_CODE(row, col, row_shift)] = code;
 	}
 
 	return 0;
 }
+EXPORT_SYMBOL_GPL(matrix_keypad_parse_properties);
+
+static int matrix_keypad_parse_keymap(const char *propname,
+				      unsigned int rows, unsigned int cols,
+				      struct input_dev *input_dev,
+				      unsigned short *keymap)
+{
+	struct device *dev = input_dev->parent;
+	struct device_node *np = dev_of_node(dev);
+	unsigned int row_shift = get_count_order(cols);
+	unsigned int max_keys = rows << row_shift;
+	u32 *keys;
+	int i;
+	int size;
+	int error;
+
+	if (!np)
+		return -ENODEV;
+
+	if (!propname)
+		propname = "linux,keymap";
+
+	size = of_property_count_elems_of_size(np, propname, sizeof(u32));
+	if (size <= 0) {
+		dev_err(dev, "missing or malformed property %s: %d\n",
+			propname, size);
+		return size < 0 ? size : -EINVAL;
+	}
+
+	if (size > max_keys) {
+		dev_err(dev, "%s size overflow (%d vs max %u)\n",
+			propname, size, max_keys);
+		return -EINVAL;
+	}
+
+	keys = kmalloc_array(size, sizeof(*keys), GFP_KERNEL);
+	if (!keys)
+		return -ENOMEM;
+
+	error = of_property_read_u32_array(np, propname, keys, size);
+	if (error) {
+		dev_err(dev, "failed to read %s property: %d\n",
+			propname, error);
+		goto out;
+	}
+
+	for (i = 0; i < size; i++) {
+		if (!matrix_keypad_map_key(input_dev, rows, cols,
+					   row_shift, keys[i], keymap)) {
+			error = -EINVAL;
+			goto out;
+		}
+	}
+
+out:
+	free(keys);
+	return error;
+}
+
 /**
  * matrix_keypad_build_keymap - convert platform keymap into matrix keymap
  * @keymap_data: keymap supplied by the platform code
- * @row_shift: number of bits to shift row value by to advance to the next
- * line in the keymap
+ * @keymap_name: name of device tree property containing keymap (if device
+ *	tree support is enabled).
+ * @rows: number of rows in target keymap array
+ * @cols: number of cols in target keymap array
  * @keymap: expanded version of keymap that is suitable for use by
- * matrix keyboad driver
+ * matrix keyboard driver
+ * @input_dev: input devices for which we are setting up the keymap
+ *
  * This function converts platform keymap (encoded with KEY() macro) into
  * an array of keycodes that is suitable for using in a standard matrix
  * keyboard driver that uses row and col as indices.
+ *
+ * If @keymap_data is not supplied and device tree support is enabled
+ * it will attempt load the keymap from property specified by @keymap_name
+ * argument (or "linux,keymap" if @keymap_name is %NULL).
+ *
+ * If @keymap is %NULL the function will automatically allocate managed
+ * block of memory to store the keymap. This memory will be associated with
+ * the parent device and automatically freed when device unbinds from the
+ * driver.
+ *
+ * Callers are expected to set up input_dev->dev.parent before calling this
+ * function.
  */
-int matrix_keypad_build_keymap(struct device *dev,
-			       const struct matrix_keymap_data *keymap_data,
-			       unsigned int row_shift,
-			       unsigned short *keymap)
+int matrix_keypad_build_keymap(const struct matrix_keymap_data *keymap_data,
+			       const char *keymap_name,
+			       unsigned int rows, unsigned int cols,
+			       unsigned short *keymap,
+			       struct input_dev *input_dev)
 {
+	unsigned int row_shift = get_count_order(cols);
+	size_t max_keys = rows << row_shift;
 	int i;
+	int error;
 
-	if (IS_ENABLED(CONFIG_OFDEVICE) && dev->of_node)
-		return matrix_keypad_parse_of_keymap(dev, row_shift, keymap);
-
-	if (!keymap_data)
+	if (WARN_ON(!input_dev->parent))
 		return -EINVAL;
 
-	for (i = 0; i < keymap_data->keymap_size; i++) {
-		unsigned int key = keymap_data->keymap[i];
-		unsigned int row = KEY_ROW(key);
-		unsigned int col = KEY_COL(key);
-		unsigned short code = KEY_VAL(key);
+	if (!keymap) {
+		keymap = devm_kcalloc(input_dev->parent,
+				      max_keys, sizeof(*keymap),
+				      GFP_KERNEL);
+		if (!keymap) {
+			dev_err(input_dev->parent,
+				"Unable to allocate memory for keymap");
+			return -ENOMEM;
+		}
+	}
 
-		keymap[MATRIX_SCAN_CODE(row, col, row_shift)] = code;
+	input_dev->keycode = keymap;
+	input_dev->keycodesize = sizeof(*keymap);
+	input_dev->keycodemax = max_keys;
+
+	if (keymap_data) {
+		for (i = 0; i < keymap_data->keymap_size; i++) {
+			unsigned int key = keymap_data->keymap[i];
+
+			if (!matrix_keypad_map_key(input_dev, rows, cols,
+						   row_shift, key, keymap))
+				return -EINVAL;
+		}
+	} else {
+		error = matrix_keypad_parse_keymap(keymap_name, rows, cols,
+						   input_dev, keymap);
+		if (error)
+			return error;
 	}
 
 	return 0;
 }
+EXPORT_SYMBOL(matrix_keypad_build_keymap);
+
+MODULE_DESCRIPTION("Helpers for matrix keyboard bindings");
+MODULE_LICENSE("GPL");
