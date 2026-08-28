b2136d1120b1b372539494506e5e8ee173dfabd2
Ahmad Fatoum <a.fatoum@pengutronix.de>
2026-08-28 20:46:42 +0200

    fixup! ARM: always call cache_invalidate_stale before enabling D-Cache
    The invalidate was removed from __barebox_arm_entry, so it precedes MMU
    setup. This was missed for ARM64 and it happened even when the MMU was
    already enabled.
    
    Additionally, the invalidate was done even if MMU was already enabled.
    We have this check for barebox as EFI payload, where we shouldn't really
    do any cache maintenance ourselves, so move it after the check.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

--- arch/arm/cpu/mmu_32.c
+++ arch/arm/cpu/mmu_32.c
@@ -613,10 +613,10 @@ void mmu_early_enable(unsigned long membase, unsigned long memsize)
 
 	pr_debug("enabling MMU, ttb @ 0x%p\n", ttb);
 
-	cache_invalidate_stale();
-
 	if (get_cr() & CR_M)
 		return;
+
+	cache_invalidate_stale();
 
 	set_ttbr(ttb);
 
