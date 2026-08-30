a3f/barebox

commit 37912154d88a9a94a042faf752ab30e63e3c2489

ARM: initial i.MX9 support
Link: https://lore.barebox.org/20231110125800.1901232-2-s.hauer@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/mach-imx/imx9.c
@@ -1,0 +1,173 @@
+#define pr_fmt(fmt)     "imx9: " fmt
+
+#include <init.h>
+#include <common.h>
+#include <linux/clk.h>
+#include <mach/imx/generic.h>
+#include <mach/imx/ele.h>
+#include <linux/bitfield.h>
+#include <mach/imx/imx9-regs.h>
+
+#define SPEED_GRADING_MASK GENMASK(11, 6)
+#define MARKETING_GRADING_MASK     GENMASK(5, 4)
+
+static u32 imx9_read_shadow_fuse(int fuse)
+{
+	void *ocotp = IOMEM(MX9_OCOTP_BASE_ADDR);
+
+	return readl(ocotp + 0x8000 + (fuse << 2));
+}
+
+static u32 imx9_cpu_speed_grade_hz(void)
+{
+	u32 speed, max_speed;
+	u32 val;
+
+	val = imx9_read_shadow_fuse(19);
+
+	val = FIELD_GET(SPEED_GRADING_MASK, val) & 0xf;
+
+	speed = 2300000000 - val * 100000000;
+
+	if (cpu_is_mx93())
+		max_speed = 1700000000;
+
+	/* In case the fuse of speed grade not programmed */
+	if (speed > max_speed)
+		speed = max_speed;
+
+	return speed;
+}
+
+static void imx93_set_arm_clock(void)
+{
+	struct clk *pll = clk_lookup("arm_pll");
+	struct clk *sel = clk_lookup("a55_sel");
+	struct clk *alt = clk_lookup("a55_alt");
+	u32 speed;
+
+	if (IS_ERR(pll) || IS_ERR(sel) || IS_ERR(alt)) {
+		pr_err("Failed to get clocks\n");
+		return;
+	}
+
+	speed = imx9_cpu_speed_grade_hz();
+	if (!speed)
+		return;
+
+	pr_debug("Setting CPU clock to %dMHz\n", speed / 1000000);
+
+	clk_set_parent(sel, alt);
+	clk_set_rate(pll, speed);
+	clk_set_parent(sel, pll);
+}
+
+static const bool imx93_have_npu(void)
+{
+	u32 val = imx9_read_shadow_fuse(19);
+
+	if (val & BIT(13))
+		return false;
+	else
+		return true;
+}
+
+static const int imx93_ncores(void)
+{
+	u32 val = imx9_read_shadow_fuse(19);
+
+	if (val & BIT(15))
+		return 1;
+	else
+		return 2;
+}
+
+static const int imx93_is_9x9(void)
+{
+	u32 val = imx9_read_shadow_fuse(20);
+	u32 pack_9x9_fused = BIT(4) | BIT(17) | BIT(19) | BIT(24);
+
+	if ((val & pack_9x9_fused) == pack_9x9_fused)
+		return true;
+	else
+		return false;
+}
+
+#define TEMP_COMMERCIAL         0
+#define TEMP_EXTCOMMERCIAL      1
+#define TEMP_INDUSTRIAL         2
+#define TEMP_AUTOMOTIVE         3
+
+static void imx93_cpu_temp_grade(int *minc, int *maxc, char *code)
+{
+	u32 val = imx9_read_shadow_fuse(19);
+	int min, max;
+	char c;
+
+	switch (FIELD_GET(MARKETING_GRADING_MASK, val)) {
+	case TEMP_AUTOMOTIVE:
+		min = -40;
+		max = 125;
+		c = 'A';
+		break;
+	case TEMP_INDUSTRIAL:
+		min = -40;
+		max = 105;
+		c = 'C';
+		break;
+	case TEMP_EXTCOMMERCIAL:
+		if (cpu_is_mx93()) {
+			/* imx93 only has extended industrial*/
+			min = -40;
+			max = 125;
+			c = 'X';
+		} else {
+			min = -20;
+			max = 105;
+			c = '-';
+		}
+		break;
+	case TEMP_COMMERCIAL:
+		min = 0;
+		max = 95;
+		c = 'D';
+		break;
+	}
+
+	if (minc)
+		*minc = min;
+	if (maxc)
+		*maxc = max;
+	if (code)
+		*code = c;
+}
+
+static void imx93_type(void)
+{
+	int subfamily, min, max;
+	char code;
+
+	if (imx93_is_9x9()) {
+		if (imx93_have_npu())
+			subfamily = 2;
+		else
+			subfamily = 1;
+	} else {
+		if (imx93_have_npu())
+			subfamily = 5;
+		else
+			subfamily = 3;
+	}
+
+	imx93_cpu_temp_grade(&min, &max, &code);
+
+	pr_info("Detected IMX93%d%d%c (%d - %dC)\n", subfamily, imx93_ncores(), code, min, max);
+}
+
+int imx93_init(void)
+{
+	imx93_type();
+	imx93_set_arm_clock();
+
+	return 0;
+}

arch/arm/mach-imx/Kconfig
@@ -34,6 +34,14 @@ config RESET_IMX_SRC
 	def_bool y
 	depends on ARCH_IMX6 || ARCH_IMX50 || ARCH_IMX51 || ARCH_IMX53
 
+config ARCH_IMX_ATF
+	def_bool y
+	depends on ARCH_IMX8M || ARCH_IMX9
+
+config ARCH_IMX_ROMAPI
+	def_bool y
+	depends on ARCH_IMX8M || ARCH_IMX9
+
 #
 # PMIC configuration found on i.MX51 Babbadge board
 #
@@ -166,6 +174,19 @@ config ARCH_IMX8MP
 config ARCH_IMX8MQ
 	select ARCH_IMX8M
 	bool
+
+config ARCH_IMX9
+	bool
+
+config ARCH_IMX93
+	bool
+	select ARCH_IMX9
+	select CPU_V8
+	select PINCTRL_IMX_IOMUX_V3
+	select OFTREE
+	select COMMON_CLK_OF_PROVIDER
+	select ARM_USE_COMPRESSED_DTB
+	select ARCH_HAS_FEC_IMX
 
 config ARCH_VF610
 	bool

arch/arm/mach-imx/Makefile
@@ -18,8 +18,10 @@ lwl-$(CONFIG_ARCH_IMX6) += imx6-mmdc.o
 obj-$(CONFIG_ARCH_IMX7) += imx7.o
 obj-$(CONFIG_ARCH_VF610) += vf610.o
 obj-pbl-$(CONFIG_ARCH_IMX8M) += imx8m.o
-lwl-$(CONFIG_ARCH_IMX8M) += atf.o
-obj-pbl-$(CONFIG_ARCH_IMX8M) += romapi.o tzasc.o
+obj-$(CONFIG_ARCH_IMX9) += imx9.o
+lwl-$(CONFIG_ARCH_IMX_ATF) += atf.o
+obj-pbl-$(CONFIG_ARCH_IMX8M) += tzasc.o
+obj-pbl-$(CONFIG_ARCH_IMX_ROMAPI) += romapi.o
 obj-$(CONFIG_IMX_IIM)	+= iim.o
 obj-$(CONFIG_NAND_IMX) += nand.o
 lwl-$(CONFIG_ARCH_IMX_EXTERNAL_BOOT_NAND) += external-nand-boot.o

arch/arm/mach-imx/cpu_init.c
@@ -10,6 +10,8 @@
 #include <mach/imx/imx7-regs.h>
 #include <mach/imx/imx8mq-regs.h>
 #include <mach/imx/imx8m-ccm-regs.h>
+#include <mach/imx/imx9-regs.h>
+#include <mach/imx/trdc.h>
 #include <io.h>
 #include <asm/syscounter.h>
 #include <asm/system.h>
@@ -85,4 +87,44 @@ void imx8mq_cpu_lowlevel_init(void)
 {
 	imx8m_cpu_lowlevel_init();
 }
+
+#define CCM_AUTHEN_TZ_NS	BIT(9)
+
+#define OSCPLLa_AUTHEN(n)		(0x5030  + (n) * 0x40) /* 0..18 */
+#define CLOCK_ROOT_AUTHEN(n)		(0x30  + (n) * 0x80) /* 0..94 */
+#define LPCGa_AUTHEN(n)			(0x8030 + (n) * 0x40) /* 0..126 */
+#define GPR_SHARED0_AUTHEN(n)		(0x4810 + (n) * 0x10) /* 0..3 */
+#define SET 4
+
+#define SRC_SP_ISO_CTRL			0x10c
+
+void imx93_cpu_lowlevel_init(void)
+{
+	void __iomem *ccm = IOMEM(MX9_CCM_BASE_ADDR);
+	void __iomem *src = IOMEM(MX9_SRC_BASE_ADDR);
+	int i;
+
+	arm_cpu_lowlevel_init();
+
+	if (current_el() != 3)
+		return;
+
+	imx9_trdc_init();
+
+	imx_cpu_timer_init(IOMEM(MX9_SYSCNT_CTRL_BASE_ADDR));
+
+	for (i = 0; i <= 18; i++)
+		writel(CCM_AUTHEN_TZ_NS, ccm + OSCPLLa_AUTHEN(i) + SET);
+	for (i = 0; i <= 94; i++)
+		writel(CCM_AUTHEN_TZ_NS, ccm + CLOCK_ROOT_AUTHEN(i) + SET);
+	for (i = 0; i <= 126 ; i++)
+		writel(CCM_AUTHEN_TZ_NS, ccm + LPCGa_AUTHEN(i) + SET);
+	for (i = 0; i <= 3 ; i++)
+		writel(CCM_AUTHEN_TZ_NS, ccm + GPR_SHARED0_AUTHEN(i) + SET);
+
+	/* clear isolation for usbphy, dsi, csi*/
+	writel(0x0, src + SRC_SP_ISO_CTRL);
+
+}
+
 #endif

arch/arm/mach-imx/imx-bbu-internal.c
@@ -617,6 +617,11 @@ int imx8m_bbu_internal_mmcboot_register_handler(const char *name,
 						 unsigned long flags)
 	__alias(imx_bbu_internal_mmcboot_register_handler);
 
+int imx9_bbu_internal_mmcboot_register_handler(const char *name,
+						 const char *devicefile,
+						 unsigned long flags)
+	__alias(imx_bbu_internal_mmcboot_register_handler);
+
 /*
  * Register an i.MX53 internal boot update handler for i2c/spi
  * EEPROMs / flashes. Nearly the same as MMC/SD, but we do not need to

arch/arm/mach-imx/imx.c
@@ -75,6 +75,8 @@ static int imx_soc_from_dt(void)
 		return IMX_CPU_IMX8MN;
 	if (of_machine_is_compatible("fsl,imx8mp"))
 		return IMX_CPU_IMX8MP;
+	if (of_machine_is_compatible("fsl,imx93"))
+		return IMX_CPU_IMX93;
 	if (of_machine_is_compatible("fsl,vf610"))
 		return IMX_CPU_VF610;
 
@@ -123,6 +125,8 @@ static int imx_init(void)
 		ret = imx8mp_init();
 	else if (cpu_is_mx8mq())
 		ret = imx8mq_init();
+	else if (cpu_is_mx93())
+		ret = imx93_init();
 	else if (cpu_is_vf610())
 		ret = vf610_init();
 	else

firmware/Kconfig
@@ -38,6 +38,9 @@ config FIRMWARE_IMX8MP_ATF
 config FIRMWARE_IMX8MQ_ATF
 	bool
 
+config FIRMWARE_IMX93_ATF
+	bool
+
 config FIRMWARE_IMX8MM_OPTEE
 	bool "install OP-TEE on i.MX8MM boards"
 	depends on FIRMWARE_IMX8MM_ATF && PBL_OPTEE

firmware/Makefile
@@ -16,6 +16,7 @@ pbl-firmware-$(CONFIG_FIRMWARE_IMX8MM_ATF) += imx8mm-bl31.bin$(if $(CONFIG_FIRMW
 pbl-firmware-$(CONFIG_FIRMWARE_IMX8MN_ATF) += imx8mn-bl31.bin$(if $(CONFIG_FIRMWARE_IMX8MN_OPTEE),-optee,)
 pbl-firmware-$(CONFIG_FIRMWARE_IMX8MP_ATF) += imx8mp-bl31.bin$(if $(CONFIG_FIRMWARE_IMX8MP_OPTEE),-optee,)
 pbl-firmware-$(CONFIG_FIRMWARE_IMX8MQ_ATF) += imx8mq-bl31.bin
+pbl-firmware-$(CONFIG_FIRMWARE_IMX93_ATF) += imx93-bl31.bin
 fw-external-$(CONFIG_FIRMWARE_IMX8MM_OPTEE) += imx8mm-bl32.bin
 fw-external-$(CONFIG_FIRMWARE_IMX8MN_OPTEE) += imx8mn-bl32.bin
 fw-external-$(CONFIG_FIRMWARE_IMX8MP_OPTEE) += imx8mp-bl32.bin

commit 1bcd80620a784e2ba1e76116533b47974e1ab272

ARM: i.MX9: initialize max_speed in imx9_cpu_speed_grade_hz
max_speed is only assigned for i.MX93:

  arch/arm/mach-imx/imx9.c:36:6: error: variable 'max_speed' is used uninitialized whenever 'if' condition is false [-Werror,-Wsometimes-uninitialized]

Initialize it to U32_MAX, so the fused speed grade is left as-is on
other SoCs.

Fixes: 37912154d88a ("ARM: initial i.MX9 support")

Assisted-by: Claude:fable-5
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

arch/arm/mach-imx/imx9.c
@@ -24,7 +24,7 @@ static u32 imx9_read_shadow_fuse(int fuse)
 
 static u32 imx9_cpu_speed_grade_hz(void)
 {
-	u32 speed, max_speed;
+	u32 speed, max_speed = U32_MAX;
 	u32 val;
 
 	val = imx9_read_shadow_fuse(19);